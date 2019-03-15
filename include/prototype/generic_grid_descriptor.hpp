89736eeabc9f0044d3c4f861181549df9a4e3f6f
Fabian Bösch <boeschf@cscs.ch>
2019-03-15 14:40:02 +0100

    topology hierarchy

---
+++ include/prototype/generic_grid_descriptor.hpp
@@ -0,0 +1,104 @@
+#ifndef INCLUDED_GENERIC_GRID_DESCRIPTOR_HPP
+#define INCLUDED_GENERIC_GRID_DESCRIPTOR_HPP
+
+#include <type_traits>
+
+
+
+namespace gridtools {
+
+// total dimensions
+// dimension of decomposed domain
+//    regular?
+// remaining dimensions of domain
+//    regular?
+//
+
+template<unsigned N, bool Regular = true>
+struct dimensions
+{
+    using rank = std::integral_constant<unsigned, N>;
+    using regular = std::integral_constant<bool, Regular>;
+};
+
+
+template<typename DecomposedDimensions, typename RemainingDimensions = dimensions<0>>
+struct grid_topology
+{
+    using decomposed_dimensions = DecomposedDimensions;
+    using remaining_dimensions  = RemainingDimensions;
+
+    // total rank/dimensionality
+    using rank = std::integral_constant<unsigned, decomposed_dimensions::rank::value + remaining_dimensions::rank::value>;
+
+    // check if at least 3D
+    static_assert(rank::value > 2, "not enough dimensions specified");
+
+
+    // fully regular grid
+    // ------------------
+    using regular = std::integral_constant<bool, 
+           decomposed_dimensions::regular::value 
+        && remaining_dimensions::regular::value>;
+    
+    // check if decomposed in 2 dimensions at most
+    static_assert(!regular::value || (decomposed_dimensions::rank::value > 0 && decomposed_dimensions::rank::value < 3),
+        "decomposition can be at least 2D for this topology");
+
+    // hypercube
+    using hypercube = std::integral_constant<bool, regular::value && (rank::value > 3)>;
+
+
+    // fully unstructured grid
+    // -----------------------
+    using unstructured = std::integral_constant<bool, 
+           !decomposed_dimensions::regular::value 
+        && remaining_dimensions::rank::value==0>;
+
+    // check if decomposed in 3D
+    static_assert(!unstructured::value || rank::value==3, "invalid unstructured grid");
+
+
+    // mixed topology
+    // --------------
+    using mixed = std::integral_constant<bool, 
+           !decomposed_dimensions::regular::value 
+        && decomposed_dimensions::rank::value==2 
+        && rank::value==3 
+        && remaining_dimensions::regular::value>;
+
+    // check if valid topology
+    static_assert( regular::value || unstructured::value || mixed::value , "invalid topology specified");
+    static_assert( 
+           ( regular::value && !unstructured::value && !mixed::value)
+        || (!regular::value &&  unstructured::value && !mixed::value)
+        || (!regular::value && !unstructured::value &&  mixed::value)        
+        , "invalid topology specified");
+};
+
+
+template<unsigned DD, unsigned RD = 1>
+using regular_grid_topology = grid_topology< dimensions<DD>, dimensions<RD> >;
+
+using mixed_grid_topology = grid_topology< dimensions<2,false>, dimensions<1> >;
+
+using unstructured_grid_topology = grid_topology< dimensions<3,false>, dimensions<0> >;
+
+
+template<unsigned Faces>
+struct inflated_regular_topology : public regular_grid_topology<2,1>
+{
+    using nested = std::true_type;
+    using face_topology = regular_grid_topology<2,1>;
+    using faces = std::integral_constant<unsigned, Faces>;
+};
+
+using inflated_cube_topology = inflated_regular_topology<6>;
+
+using inflated_icosahedron_topology = inflated_regular_topology<10>;
+
+
+}
+
+
+#endif /* INCLUDED_GENERIC_GRID_DESCRIPTOR_HPP */
---
+++ tests/generic_grid_descriptor.cpp
@@ -0,0 +1,66 @@
+
+
+
+#include "../include/prototype/generic_grid_descriptor.hpp"
+#include <iostream>
+
+using namespace gridtools;
+
+
+// regular grid
+using reg_dim_4 = dimensions<4,true>;
+using reg_dim_3 = dimensions<3,true>;
+using reg_dim_2 = dimensions<2,true>;
+using reg_dim_1 = dimensions<1,true>;
+using reg_dim_0 = dimensions<0,true>;
+using regular_topology_12 = grid_topology<reg_dim_1,reg_dim_2>;
+using regular_topology_21 = grid_topology<reg_dim_2,reg_dim_1>;
+using regular_topology_13 = grid_topology<reg_dim_1,reg_dim_3>;
+using regular_topology_22 = grid_topology<reg_dim_2,reg_dim_2>;
+using regular_topology_14 = grid_topology<reg_dim_1,reg_dim_4>;
+using regular_topology_23 = grid_topology<reg_dim_2,reg_dim_3>;
+
+// unstructured grid
+using dim_4 = dimensions<4,false>;
+using dim_3 = dimensions<3,false>;
+using dim_2 = dimensions<2,false>;
+using dim_1 = dimensions<1,false>;
+using unstructured_topology = grid_topology<dim_3>;
+using unstructured_topology_21 = grid_topology<dim_2,dim_1>;
+using unstructured_topology_4 = grid_topology<dim_4>;
+
+// mixed topology
+using mixed_topology = grid_topology<dim_2,reg_dim_1>;
+
+
+
+int main()
+{
+    regular_topology_12 r_12;
+    regular_topology_21 r_21;
+    regular_topology_13 r_13;
+    regular_topology_22 r_22;
+    regular_topology_14 r_14;
+    regular_topology_23 r_23;
+
+    unstructured_topology u;
+
+    mixed_topology m;
+
+    regular_grid_topology<1,2> rgt0;
+    regular_grid_topology<2,1> rgt1;
+    //regular_grid_topology<3,0> rgt2;
+    //regular_grid_topology<4,0> rgt3;
+
+    unstructured_grid_topology u_;
+
+    mixed_grid_topology m_;
+
+
+    inflated_cube_topology ic;
+
+
+    std::cout << inflated_cube_topology::rank::value << std::endl;
+
+    return 0;
+}
