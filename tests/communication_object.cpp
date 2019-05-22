66b4d74913b07f94b46b5f14747984223b14e38d
bettiolm <bettiolm@cscs.ch>
2019-05-22 18:00:52 +0200

    added first test

---
+++ tests/communication_object.cpp
@@ -0,0 +1,18 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2019, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <communication_object.hpp>
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+
+}
