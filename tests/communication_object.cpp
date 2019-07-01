0f0222ec1ae603708e74b649055e476ab21cb394
bettiolm <bettiolm@cscs.ch>
2019-07-01 10:43:41 +0200

    added test with multiple fileds exchange with multithreading: passed

--- tests/communication_object.cpp
+++ tests/communication_object.cpp
@@ -649,3 +649,261 @@ TEST(communication_object, multithreading) {
     EXPECT_TRUE(passed);
 
 }
+
+
+TEST(communication_object, multithreading_multiple_fileds) {
+
+    using domain_descriptor_t = gridtools::structured_domain_descriptor<int, 3>;
+    using domain_id_t = domain_descriptor_t::domain_id_type;
+    using coordinate_t = domain_descriptor_t::coordinate_type;
+    using halo_generator_t = gridtools::structured_halo_generator<domain_id_t, 3>;
+    using layout_map_type = gridtools::layout_map<2, 1, 0>;
+
+    boost::mpi::communicator world;
+    gridtools::protocol::communicator<gridtools::protocol::mpi> comm{world};
+
+    /* Problem sizes */
+    const int d1 = 2;
+    const int d2 = 2;
+    const int d3 = 1;
+    const int DIM1 = 5;
+    const int DIM2 = 5;
+    const int DIM3 = 5;
+    const int H1m = 1;
+    const int H1p = 1;
+    const int H2m = 1;
+    const int H2p = 1;
+    const int H3m = 1;
+    const int H3p = 1;
+    const std::array<int, 3> g_first{0, 0, 0};
+    const std::array<int, 3> g_last{d1 * DIM1 * 2 - 1, d2 * DIM2 - 1, d3 * DIM3 - 1};
+    const std::array<int, 6> halos{H1m, H1p, H2m, H2p, H3m, H3p};
+    const std::array<bool, 3> periodic{true, true, true};
+    int coords[3]{comm.rank() % d1, comm.rank() / d1, 0}; // rank in cartesian coordinates
+
+    std::vector<domain_descriptor_t> local_domains;
+
+    domain_descriptor_t my_domain_1{
+        comm.rank() * 2,
+        coordinate_t{(comm.rank() % d1) * DIM1 * 2               , (comm.rank() / d1    ) * DIM2    , 0     },
+        coordinate_t{(comm.rank() % d1) * DIM1 * 2 + DIM1 - 1    , (comm.rank() / d1 + 1) * DIM2 - 1, DIM3-1}
+    };
+    local_domains.push_back(my_domain_1);
+
+    domain_descriptor_t my_domain_2{
+        comm.rank() * 2 + 1,
+        coordinate_t{(comm.rank() % d1) * DIM1 * 2 + DIM1        , (comm.rank() / d1    ) * DIM2    , 0     },
+        coordinate_t{(comm.rank() % d1) * DIM1 * 2 + DIM1 * 2 - 1, (comm.rank() / d1 + 1) * DIM2 - 1, DIM3-1}
+    };
+    local_domains.push_back(my_domain_2);
+
+    auto halo_gen = halo_generator_t{g_first, g_last, halos, periodic};
+
+    auto patterns = gridtools::make_pattern<gridtools::structured_grid>(world, halo_gen, local_domains);
+
+    using communication_object_t = gridtools::communication_object<std::remove_reference_t<decltype(*(patterns.begin()))>, gridtools::cpu>;
+
+    std::vector<communication_object_t> cos;
+    for (const auto& p : patterns) {
+        cos.push_back(communication_object_t{p});
+    }
+
+    triple_t<USE_DOUBLE, int>* _values_1_1 = new triple_t<USE_DOUBLE, int>[(DIM1 + H1m + H1p) * (DIM2 + H2m + H2p) * (DIM3 + H3m + H3p)];
+    array<triple_t<USE_DOUBLE, int>, layout_map_type> values_1_1(_values_1_1, (DIM1 + H1m + H1p), (DIM2 + H2m + H2p), (DIM3 + H3m + H3p));
+    my_data_desc<triple_t<USE_DOUBLE, int>, domain_descriptor_t> data_1_1{
+        local_domains[0],
+        coordinate_t{H1m, H2m, H3m},
+        values_1_1
+    };
+
+    triple_t<USE_DOUBLE, double>* _values_1_2 = new triple_t<USE_DOUBLE, double>[(DIM1 + H1m + H1p) * (DIM2 + H2m + H2p) * (DIM3 + H3m + H3p)];
+    array<triple_t<USE_DOUBLE, double>, layout_map_type> values_1_2(_values_1_2, (DIM1 + H1m + H1p), (DIM2 + H2m + H2p), (DIM3 + H3m + H3p));
+    my_data_desc<triple_t<USE_DOUBLE, double>, domain_descriptor_t> data_1_2{
+        local_domains[0],
+        coordinate_t{H1m, H2m, H3m},
+        values_1_2
+    };
+
+    triple_t<USE_DOUBLE, int>* _values_2_1 = new triple_t<USE_DOUBLE, int>[(DIM1 + H1m + H1p) * (DIM2 + H2m + H2p) * (DIM3 + H3m + H3p)];
+    array<triple_t<USE_DOUBLE, int>, layout_map_type> values_2_1(_values_2_1, (DIM1 + H1m + H1p), (DIM2 + H2m + H2p), (DIM3 + H3m + H3p));
+    my_data_desc<triple_t<USE_DOUBLE, int>, domain_descriptor_t> data_2_1{
+        local_domains[1],
+        coordinate_t{H1m, H2m, H3m},
+        values_2_1
+    };
+
+    triple_t<USE_DOUBLE, double>* _values_2_2 = new triple_t<USE_DOUBLE, double>[(DIM1 + H1m + H1p) * (DIM2 + H2m + H2p) * (DIM3 + H3m + H3p)];
+    array<triple_t<USE_DOUBLE, double>, layout_map_type> values_2_2(_values_2_2, (DIM1 + H1m + H1p), (DIM2 + H2m + H2p), (DIM3 + H3m + H3p));
+    my_data_desc<triple_t<USE_DOUBLE, double>, domain_descriptor_t> data_2_2{
+        local_domains[1],
+        coordinate_t{H1m, H2m, H3m},
+        values_2_2
+    };
+
+    /* Just an initialization */
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+                values_1_1(ii, jj, kk) = triple_t<USE_DOUBLE, int>();
+            }
+    for (int ii = H1m; ii < DIM1 + H1m; ++ii)
+        for (int jj = H2m; jj < DIM2 + H2m; ++jj)
+            for (int kk = H3m; kk < DIM3 + H3m; ++kk) {
+                values_1_1(ii, jj, kk) = triple_t<USE_DOUBLE, int>(
+                            ii - H1m + DIM1 * 2 * coords[0],
+                            jj - H2m + DIM2     * coords[1],
+                            kk - H3m + DIM3     * coords[2]
+                        );
+            }
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+                values_1_2(ii, jj, kk) = triple_t<USE_DOUBLE, double>();
+            }
+    for (int ii = H1m; ii < DIM1 + H1m; ++ii)
+        for (int jj = H2m; jj < DIM2 + H2m; ++jj)
+            for (int kk = H3m; kk < DIM3 + H3m; ++kk) {
+                values_1_2(ii, jj, kk) = triple_t<USE_DOUBLE, double>(
+                            ii - H1m + DIM1 * 2 * coords[0],
+                            jj - H2m + DIM2     * coords[1],
+                            kk - H3m + DIM3     * coords[2]
+                        );
+            }
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+                values_2_1(ii, jj, kk) = triple_t<USE_DOUBLE, int>();
+            }
+    for (int ii = H1m; ii < DIM1 + H1m; ++ii)
+        for (int jj = H2m; jj < DIM2 + H2m; ++jj)
+            for (int kk = H3m; kk < DIM3 + H3m; ++kk) {
+                values_2_1(ii, jj, kk) = triple_t<USE_DOUBLE, int>(
+                            ii - H1m + DIM1 * 2 * coords[0] + DIM1,
+                            jj - H2m + DIM2     * coords[1]       ,
+                            kk - H3m + DIM3     * coords[2]
+                        );
+            }
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+                values_2_2(ii, jj, kk) = triple_t<USE_DOUBLE, double>();
+            }
+    for (int ii = H1m; ii < DIM1 + H1m; ++ii)
+        for (int jj = H2m; jj < DIM2 + H2m; ++jj)
+            for (int kk = H3m; kk < DIM3 + H3m; ++kk) {
+                values_2_2(ii, jj, kk) = triple_t<USE_DOUBLE, double>(
+                            ii - H1m + DIM1 * 2 * coords[0] + DIM1,
+                            jj - H2m + DIM2     * coords[1]       ,
+                            kk - H3m + DIM3     * coords[2]
+                        );
+            }
+
+    std::vector<std::thread> threads;
+
+    threads.push_back(std::thread([&cos, &data_1_1, &data_1_2](){
+        auto h1 = cos[0].exchange(data_1_1, data_1_2);
+        h1.wait();
+    }));
+
+    threads.push_back(std::thread([&cos, &data_2_1, &data_2_2](){
+        auto h2 = cos[1].exchange(data_2_1, data_2_2);
+        h2.wait();
+    }));
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    int passed = true;
+
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+
+                triple_t<USE_DOUBLE, int> t1;
+                int tx, ty, tz;
+
+                tx = modulus(ii - H1m + DIM1 * 2 * coords[0], DIM1 * 2 * 2);
+                ty = modulus(jj - H2m + DIM2     * coords[1], DIM2     * 2);
+                tz = modulus(kk - H3m + DIM3     * coords[2], DIM3        );
+
+                t1 = triple_t<USE_DOUBLE, int>(tx, ty, tz).floor();
+
+                if (values_1_1(ii, jj, kk) != t1) {
+                    passed = false;
+                    std::cout << ii << ", " << jj << ", " << kk << " values found != expected: "
+                              << "values_1_1 " << values_1_1(ii, jj, kk) << " != " << t1 << "\n";
+
+                }
+
+            }
+
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+
+                triple_t<USE_DOUBLE, double> t1;
+                int tx, ty, tz;
+
+                tx = modulus(ii - H1m + DIM1 * 2 * coords[0], DIM1 * 2 * 2);
+                ty = modulus(jj - H2m + DIM2     * coords[1], DIM2     * 2);
+                tz = modulus(kk - H3m + DIM3     * coords[2], DIM3        );
+
+                t1 = triple_t<USE_DOUBLE, double>(tx, ty, tz).floor();
+
+                if (values_1_2(ii, jj, kk) != t1) {
+                    passed = false;
+                    std::cout << ii << ", " << jj << ", " << kk << " values found != expected: "
+                              << "values_1_2 " << values_1_2(ii, jj, kk) << " != " << t1 << "\n";
+
+                }
+
+            }
+
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+
+                triple_t<USE_DOUBLE, int> t2;
+                int tx, ty, tz;
+
+                tx = modulus(ii - H1m + DIM1 * 2 * coords[0] + DIM1, DIM1 * 2 * 2);
+                ty = modulus(jj - H2m + DIM2     * coords[1]       , DIM2     * 2);
+                tz = modulus(kk - H3m + DIM3     * coords[2]       , DIM3        );
+
+                t2 = triple_t<USE_DOUBLE, int>(tx, ty, tz).floor();
+
+                if (values_2_1(ii, jj, kk) != t2) {
+                    passed = false;
+                    std::cout << ii << ", " << jj << ", " << kk << " values found != expected: "
+                              << "values_2_1 " << values_2_1(ii, jj, kk) << " != " << t2 << "\n";
+
+                }
+
+            }
+
+    for (int ii = 0; ii < DIM1 + H1m + H1p; ++ii)
+        for (int jj = 0; jj < DIM2 + H2m + H2p; ++jj)
+            for (int kk = 0; kk < DIM3 + H3m + H3p; ++kk) {
+
+                triple_t<USE_DOUBLE, double> t2;
+                int tx, ty, tz;
+
+                tx = modulus(ii - H1m + DIM1 * 2 * coords[0] + DIM1, DIM1 * 2 * 2);
+                ty = modulus(jj - H2m + DIM2     * coords[1]       , DIM2     * 2);
+                tz = modulus(kk - H3m + DIM3     * coords[2]       , DIM3        );
+
+                t2 = triple_t<USE_DOUBLE, double>(tx, ty, tz).floor();
+
+                if (values_2_2(ii, jj, kk) != t2) {
+                    passed = false;
+                    std::cout << ii << ", " << jj << ", " << kk << " values found != expected: "
+                              << "values_2 " << values_2_2(ii, jj, kk) << " != " << t2 << "\n";
+
+                }
+
+            }
+
+    EXPECT_TRUE(passed);
+
+}
