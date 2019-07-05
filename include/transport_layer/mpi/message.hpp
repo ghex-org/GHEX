c4b61752de5b9c9bf3bab12eb4107de2f99b7af5
Mauro Bianco <mbianco@cscs.ch>
2019-07-05 21:24:46 +0200

    more comments and cleanup

--- include/transport_layer/mpi/message.hpp
+++ include/transport_layer/mpi/message.hpp
@@ -156,15 +156,45 @@ namespace mpi {
         }
     };
 
+
+    /** shared_message is a class that represents a buffer of bytes. Each transport
+     *  layer will need (potentially) a different type of message.
+     *
+     * A shared_message can be resized.
+     *
+     * A shared_message can exists in multiple instances that are in fact
+     * shared (it's a shared_ptr). The different copies of the shared_message
+     * are reference counted, so that there are no lifetime concerns.
+     *
+     * The capacity indicates the size of the allocated storage, while the
+     * size indicates the amnount bytes used in the message
+     *
+     * The intended use is to fill the message with data using enqueue or at<>
+     * and then send it, or receive it and then access it.
+     *
+     * @tparam Allocator Allocator used by the message
+     */
     template <typename Allocator = std::allocator<unsigned char> >
     struct shared_message {
         std::shared_ptr<message<Allocator>> m_s_message;
 
+        /** Constructor that take capacity and allocator. Size is kept to 0
+         *
+         * @param capacity Capacity
+         * @param alloc Allocator instance
+         */
         shared_message(size_t capacity, Allocator allc = Allocator{})
             : m_s_message{std::make_shared<message<Allocator>>(capacity, allc)}
             {}
 
-        shared_message(size_t capacity, size_t size, Allocator allc = Allocator{})
+        /** Constructor that take capacity size and allocator.
+         * The size elements are un-initialzed. Requires size>=capacity.
+         *
+         * @param capacity Capacity
+         * @param size
+         * @param alloc Allocator instance
+         */
+         shared_message(size_t capacity, size_t size, Allocator allc = Allocator{})
             : m_s_message{std::make_shared<message<Allocator>>(capacity, size, allc)}
             {}
 
@@ -179,10 +209,16 @@ namespace mpi {
          * @return Pointer to the beginning of the message
          */
         unsigned char* data() const {
-            return (*m_s_message).data();
+            return m_s_message->data();
         }
 
+        /** Returns the number of owners of this shared_message */
         long use_count() const { return m_s_message.use_count(); }
+
+        /** Checks if the message contains a message or if it has beed deallocated */
+        bool is_valid() const { return m_s_message; }
+
+
         /** This is the main function used by the communicator to access the
          * size of the message to send or receive. This is done so that a std::vector
          * could be used as message.
@@ -190,24 +226,24 @@ namespace mpi {
          * @return current size
          */
         size_t size() const {
-            return (*m_s_message).size();
+            return m_s_message->size();
         }
 
 
         /** Reset the size of the message to 0 */
         void empty() {
-            (*m_s_message).empty();
+            m_s_message->empty();
         }
 
-        size_t capacity() const { return (*m_s_message).data(); }
+        size_t capacity() const { return m_s_message->data(); }
 
         /** Simple iterator facility to read the bytes out of the message */
-        unsigned char* begin() { return (*m_s_message).begin(); }
-        unsigned char* end() const { return (*m_s_message).end(); }
+        unsigned char* begin() { return m_s_message->begin(); }
+        unsigned char* end() const { return m_s_message->end(); }
 
         /** Function to resize a message to a new capacity. Size is unchanged */
         void resize(size_t new_capacity) {
-            (*m_s_message).resize(new_capacity);
+            m_s_message->resize(new_capacity);
         }
 
         /** Function to add an element of type T at the end of the message.
@@ -220,7 +256,7 @@ namespace mpi {
          */
         template <typename T>
         void enqueue(T x) {
-            (*m_s_message).enqueue(x);
+            m_s_message->enqueue(x);
         }
 
         /** Function to access an element of type T at position pos in the message.
@@ -236,7 +272,7 @@ namespace mpi {
          */
         template <typename T>
         T& at(size_t pos /* in bytes */ ) const {
-            return (*m_s_message). template at<T>(pos);
+            return m_s_message-> template at<T>(pos);
         }
     };
 } //namespace mpi
