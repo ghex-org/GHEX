305bc97aacb29a8e029b7248ed8a1ae203a4f906
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2019-07-23 13:51:13 +0200

    byte strides for correct stride-1 alignment

--- include/simple_field_wrapper.hpp
+++ include/simple_field_wrapper.hpp
@@ -37,11 +37,11 @@ namespace gridtools {
         for (std::size_t i=0u; i<D; ++i) a[i] -= b[i];
         return std::move(a);
     }
-    template <typename T, size_t D>
+    template <typename T, typename U, size_t D>
     GT_FUNCTION
-    T dot(const array<T,D>& a, const array<T,D>& b)
+    typename std::common_type<T, U>::type dot(const array<T,D>& a, const array<U,D>& b)
     {
-        T res = a[0]*b[0];
+        typename std::common_type<T, U>::type res = a[0]*b[0];
         for (std::size_t i=1u; i<D; ++i) res+=a[i]*b[i];
         return res;
     }
@@ -50,9 +50,9 @@ namespace gridtools {
         template<int D, int I>
         struct compute_strides_impl
         {
-            template<typename Layout, typename Coordinate>
+            template<typename Layout, typename Coordinate, typename Strides>
             GT_FUNCTION
-            static void apply(const Coordinate& extents, Coordinate& strides)
+            static void apply(const Coordinate& extents, Strides& strides)
             {
                 const auto last_idx = Layout::template find<I>();
                 const auto idx      = Layout::template find<I-1>();
@@ -63,9 +63,9 @@ namespace gridtools {
         template<int D>
         struct compute_strides_impl<D,0>
         {
-            template<typename Layout, typename Coordinate>
+            template<typename Layout, typename Coordinate, typename Strides>
             GT_FUNCTION
-            static void apply(const Coordinate&, Coordinate&)
+            static void apply(const Coordinate&, Strides&)
             {
             }
         };
@@ -79,6 +79,32 @@ namespace gridtools {
                 const auto idx      = Layout::template find<D-1>();
                 strides[idx]        = 1;
                 compute_strides_impl<D,D-1>::template apply<Layout>(extents,strides);
+            }
+            template<typename Layout, typename T, typename Coordinate, typename Strides>
+            GT_FUNCTION
+            static void apply(const Coordinate& extents, Strides& strides, std::size_t padding)
+            {
+                const auto idx      = Layout::template find<D-1>();
+                strides[idx]        = sizeof(T);
+                const auto idx2     = Layout::template find<D-2>();
+                strides[idx2]       = sizeof(T)*extents[idx] + padding; 
+                compute_strides_impl<D,D-2>::template apply<Layout>(extents,strides);
+            }
+        };
+        template<>
+        struct compute_strides<1>
+        {
+            template<typename Layout, typename Coordinate>
+            GT_FUNCTION
+            static void apply(const Coordinate&, Coordinate& strides)
+            {
+                strides[0]        = 1;
+            }
+            template<typename Layout, typename T, typename Coordinate, typename Strides>
+            GT_FUNCTION
+            static void apply(const Coordinate&, Strides& strides, std::size_t)
+            {
+                strides[0]        = sizeof(T);
             }
         };
 
@@ -120,11 +146,13 @@ namespace gridtools {
     
 
 #ifdef __CUDACC__
-    template<typename Layout, typename T, std::size_t D, typename I>
+    //template<typename Layout, typename T, std::size_t D, typename I>
+    template<typename Layout, typename T, std::size_t D, typename I, typename S>
     __global__ void pack_kernel(int size, const T* data, T* buffer, 
                                 //array<I,D> local_first, array<I,D> local_last, 
                                 array<I,D> local_first, array<I,D> local_strides,
-                                array<I,D> extents, array<I,D> offsets, array<I,D> strides)
+                                //array<I,D> extents, array<I,D> offsets, array<I,D> strides)
+                                array<S,D> byte_strides, array<I,D> offsets)
     {
         const auto index = blockIdx.x*blockDim.x + threadIdx.x;
         if (index < size)
@@ -140,18 +168,22 @@ namespace gridtools {
             // add offset
             const auto memory_coordinate = local_coordinate + local_first + offsets;
             // multiply with memory strides
-            const auto idx = dot(memory_coordinate, strides);
+            //const auto idx = dot(memory_coordinate, strides);
+            const auto idx = dot(memory_coordinate, byte_strides);
             //const decltype(idx) mm = extents[0]*extents[1]*extents[2]-1;
             //const auto idx2 = idx < mm ? idx : mm;
-            buffer[index] = data[idx];
+            //buffer[index] = data[idx];
+            buffer[index] = *reinterpret_cast<const T*>((const char*)data + idx);
         }
     }
 
-    template<typename Layout, typename T, std::size_t D, typename I>
+    //template<typename Layout, typename T, std::size_t D, typename I>
+    template<typename Layout, typename T, std::size_t D, typename I, typename S>
     __global__ void unpack_kernel(int size, T* data, const T* buffer, 
                                 //array<I,D> local_first, array<I,D> local_last, 
                                 array<I,D> local_first, array<I,D> local_strides,
-                                array<I,D> extents, array<I,D> offsets, array<I,D> strides)
+                                //array<I,D> extents, array<I,D> offsets, array<I,D> strides)
+                                array<S,D> byte_strides, array<I,D> offsets)
     {
         const auto index = blockIdx.x*blockDim.x + threadIdx.x;
         if (index < size)
@@ -167,8 +199,10 @@ namespace gridtools {
             // add offset
             const auto memory_coordinate = local_coordinate + local_first + offsets;
             // multiply with memory strides
-            const auto idx = dot(memory_coordinate, strides);
-            data[idx] = buffer[index];
+            //const auto idx = dot(memory_coordinate, strides);
+            const auto idx = dot(memory_coordinate, byte_strides);
+            //data[idx] = buffer[index];
+            *reinterpret_cast<T*>((char*)data + idx) = buffer[index];
         }
     }
 #endif
@@ -178,40 +212,54 @@ namespace gridtools {
     //template<typename Dimension, typename Layout>
     //struct serialization<device::cpu, Dimension, Layout>
     {
-        template<typename T, typename IndexContainer, typename Array>
+        //template<typename T, typename IndexContainer, typename Array>
+        template<typename T, typename IndexContainer, typename Strides, typename Array>
         GT_FUNCTION_HOST
-        static void pack(T* buffer, const IndexContainer& c, const T* m_data, const Array& m_extents, const Array& m_offsets, const Array& /*m_strides*/, void*)
+        //static void pack(T* buffer, const IndexContainer& c, const T* m_data, const Array& m_extents, 
+        //                 const Array& m_offsets, const Array& /*m_strides*/, void*)
+        static void pack(T* buffer, const IndexContainer& c, const T* m_data, const Strides& m_byte_strides, 
+                         const Array& m_offsets, void*)
         {
             for (const auto& is : c)
             {
                 detail::for_loop_pointer_arithmetic<Dimension::value,Dimension::value,Layout>::apply(
                     [m_data,buffer](auto o_data, auto o_buffer)
                     {
-                        buffer[o_buffer] = m_data[o_data]; 
+                        //buffer[o_buffer] = m_data[o_data]; 
+                        *reinterpret_cast<T*>(reinterpret_cast<char*>(buffer)+o_buffer) = 
+                        *reinterpret_cast<const T*>(reinterpret_cast<const char*>(m_data)+o_data); 
                     }, 
                     is.local().first(), 
                     is.local().last(),
-                    m_extents,
+                    //m_extents,
+                    m_byte_strides,
                     m_offsets
                     );
                 buffer += is.size();
             }
         }
 
-        template<typename T, typename IndexContainer, typename Array>
+        //template<typename T, typename IndexContainer, typename Array>
+        template<typename T, typename IndexContainer, typename Strides, typename Array>
         GT_FUNCTION_HOST
-        static void unpack(const T* buffer, const IndexContainer& c, T* m_data, const Array& m_extents, const Array& m_offsets, const Array& /*m_strides*/, void*)
+        //static void unpack(const T* buffer, const IndexContainer& c, T* m_data, const Array& m_extents, 
+        //                   const Array& m_offsets, const Array& /*m_strides*/, void*)
+        static void unpack(const T* buffer, const IndexContainer& c, T* m_data, const Strides& m_byte_strides, 
+                           const Array& m_offsets, void*)
         {
             for (const auto& is : c)
             {
                 detail::for_loop_pointer_arithmetic<Dimension::value,Dimension::value,Layout>::apply(
                     [m_data,buffer](auto o_data, auto o_buffer)
                     {
-                        m_data[o_data] = buffer[o_buffer];
+                        //m_data[o_data] = buffer[o_buffer];
+                        *reinterpret_cast<T*>(reinterpret_cast<char*>(m_data)+o_data) = 
+                        *reinterpret_cast<const T*>(reinterpret_cast<const char*>(buffer)+o_buffer); 
                     }, 
                     is.local().first(), 
                     is.local().last(),
-                    m_extents,
+                    //m_extents,
+                    m_byte_strides,
                     m_offsets
                     );
                 buffer += is.size();
@@ -224,9 +272,13 @@ namespace gridtools {
     template<typename Dimension, typename Layout>
     struct serialization<device::gpu, Dimension, Layout>
     {
-        template<typename T, typename IndexContainer, typename Array>
+        //template<typename T, typename IndexContainer, typename Array>
+        template<typename T, typename IndexContainer, typename Strides, typename Array>
         GT_FUNCTION_HOST
-        static void pack(T* buffer, const IndexContainer& c, const T* m_data, const Array& m_extents, const Array& m_offsets, const Array& m_strides, void* arg)
+        //static void pack(T* buffer, const IndexContainer& c, const T* m_data, const Array& m_extents, 
+        //                 const Array& m_offsets, const Array& m_strides, void* arg)
+        static void pack(T* buffer, const IndexContainer& c, const T* m_data, const Strides& m_byte_strides, 
+                         const Array& m_offsets, void* arg)
         {
             auto stream_ptr = reinterpret_cast<cudaStream_t*>(arg);
             for (const auto& is : c)
@@ -242,14 +294,19 @@ namespace gridtools {
                 //pack_kernel<Layout><<<(size+NCTIS-1)/NCTIS,NCTIS>>>(
                 //  size, m_data, buffer, local_first, local_last, m_extents, m_offsets, m_strides);
                 pack_kernel<Layout><<<(size+NCTIS-1)/NCTIS,NCTIS,0,*stream_ptr>>>(
-                    size, m_data, buffer, local_first, local_strides, m_extents, m_offsets, m_strides);
+                    //size, m_data, buffer, local_first, local_strides, m_extents, m_offsets, m_strides);
+                    size, m_data, buffer, local_first, local_strides, m_byte_strides, m_offsets);
                 buffer += size;
             }
         }
 
-        template<typename T, typename IndexContainer, typename Array>
+        //template<typename T, typename IndexContainer, typename Array>
+        template<typename T, typename IndexContainer, typename Strides, typename Array>
         GT_FUNCTION_HOST
-        static void unpack(const T* buffer, const IndexContainer& c, T* m_data, const Array& m_extents, const Array& m_offsets, const Array& m_strides, void* arg)
+        //static void unpack(const T* buffer, const IndexContainer& c, T* m_data, const Array& m_extents, 
+        //                   const Array& m_offsets, const Array& m_strides, void* arg)
+        static void unpack(const T* buffer, const IndexContainer& c, T* m_data, const Strides& m_byte_strides, 
+                           const Array& m_offsets, void* arg)
         {
             auto stream_ptr = reinterpret_cast<cudaStream_t*>(arg);
             for (const auto& is : c)
@@ -267,7 +324,8 @@ namespace gridtools {
                 //unpack_kernel<Layout><<<(size+NCTIS-1)/NCTIS,NCTIS>>>(
                 //        size, m_data, buffer, local_first, local_strides, m_extents, m_offsets, m_strides);
                 unpack_kernel<Layout><<<(size+NCTIS-1)/NCTIS,NCTIS,0,*stream_ptr>>>(
-                        size, m_data, buffer, local_first, local_strides, m_extents, m_offsets, m_strides);
+                        //size, m_data, buffer, local_first, local_strides, m_extents, m_offsets, m_strides);
+                        size, m_data, buffer, local_first, local_strides, m_byte_strides, m_offsets);
                 buffer += size;
             }
         }
@@ -299,14 +357,16 @@ namespace gridtools {
         using domain_id_type         = DomainIdType;
         //using coordinate_type        = typename domain_descriptor_type::halo_generator_type::coordinate_type;
         using coordinate_type        = array<typename domain_descriptor_type::halo_generator_type::coordinate_type::element_type, dimension::value>;
+        using strides_type           = array<std::size_t, dimension::value>;
 
     private: // members
         domain_id_type  m_dom_id;
         value_type*     m_data;
-        coordinate_type m_strides;
+        //coordinate_type m_strides;
         coordinate_type m_offsets;
         coordinate_type m_extents;
         device_id_type  m_device_id;
+        strides_type    m_byte_strides;
 
     public: // ctors
         /** @brief construcor 
@@ -318,12 +378,13 @@ namespace gridtools {
         template<typename Array>
         GT_FUNCTION_HOST
         simple_field_wrapper(domain_id_type dom_id, value_type* data, const Array& offsets, const Array& extents, device_id_type d_id = 0)
-        : m_dom_id(dom_id), m_data(data), /*m_strides(1),*/ m_device_id(d_id)
+        : m_dom_id(dom_id), m_data(data), m_device_id(d_id)
         { 
             std::copy(offsets.begin(), offsets.end(), m_offsets.begin());
             std::copy(extents.begin(), extents.end(), m_extents.begin());
             // compute strides
-            detail::compute_strides<dimension::value>::template apply<layout_map>(m_extents,m_strides);
+            //detail::compute_strides<dimension::value>::template apply<layout_map>(m_extents,m_strides);
+            detail::compute_strides<dimension::value>::template apply<layout_map,value_type>(m_extents,m_byte_strides,0u);
         }
 
         template<typename Array>
@@ -334,15 +395,14 @@ namespace gridtools {
             std::copy(offsets.begin(), offsets.end(), m_offsets.begin());
             std::copy(extents.begin(), extents.end(), m_extents.begin());
             // pad stride 1 dimension
-            auto ext_1 = m_extents[layout_map::template find<dimension::value-1>()];
-            ext_1 = (((sizeof(value_type)*ext_1+255)/256) * 256)/sizeof(value_type);
-            m_extents[layout_map::template find<dimension::value-1>()] = ext_1;
+            const auto ext_1 = m_extents[layout_map::template find<dimension::value-1>()]*sizeof(value_type);
+            const std::size_t padding = ((ext_1+255)/256)*256 - ext_1;
             // compute strides
-            detail::compute_strides<dimension::value>::template apply<layout_map>(m_extents,m_strides);
-            // compute index of the first physical point
-            auto stride_origin = dot(m_offsets, m_strides);
+            detail::compute_strides<dimension::value>::template apply<layout_map,value_type>(m_extents,m_byte_strides,padding);
+            // compute offset in bytes of the first physical point
+            auto offset_origin = dot(m_offsets, m_byte_strides);
             // compute pointer value of first physical point 
-            auto ptr_origin = reinterpret_cast<std::uintptr_t>((void*)data) + stride_origin*sizeof(value_type); 
+            auto ptr_origin = reinterpret_cast<std::uintptr_t>((void*)data) + offset_origin; 
             // compute delta
             const auto delta = ((ptr_origin + 255)/256) * 256 - ptr_origin;
             // adjust base pointer
@@ -371,30 +431,36 @@ namespace gridtools {
          * @param x coordinate vector with respect to offset specified in constructor
          * @return reference to value */
         GT_FUNCTION
-        value_type& operator()(const coordinate_type& x) { return m_data[dot(x,m_strides)]; }
+        //value_type& operator()(const coordinate_type& x) { return m_data[dot(x,m_strides)]; }
+        value_type& operator()(const coordinate_type& x) { return *reinterpret_cast<T*>((char*)m_data +dot(x,m_byte_strides)); }
         GT_FUNCTION
-        const value_type& operator()(const coordinate_type& x) const { return m_data[dot(x,m_strides)]; }
+        //const value_type& operator()(const coordinate_type& x) const { return m_data[dot(x,m_strides)]; }
+        const value_type& operator()(const coordinate_type& x) const { return *reinterpret_cast<const T*>((const char*)m_data +dot(x,m_byte_strides)); }
 
         /** @brief access operator
          * @param is coordinates with respect to offset specified in constructor
          * @return reference to value */
         template<typename... Is>
         GT_FUNCTION
-        value_type& operator()(Is&&... is) { return m_data[dot(coordinate_type{is...}+m_offsets,m_strides)]; }
+        //value_type& operator()(Is&&... is) { return m_data[dot(coordinate_type{is...}+m_offsets,m_strides)]; }
+        value_type& operator()(Is&&... is) { return *reinterpret_cast<T*>((char*)m_data+dot(coordinate_type{is...}+m_offsets,m_byte_strides)); }
         template<typename... Is>
         GT_FUNCTION
-        const value_type& operator()(Is&&... is) const { return m_data[dot(coordinate_type{is...}+m_offsets,m_strides)]; }
+        //const value_type& operator()(Is&&... is) const { return m_data[dot(coordinate_type{is...}+m_offsets,m_strides)]; }
+        const value_type& operator()(Is&&... is) const { return *reinterpret_cast<const T*>((const char*)m_data+dot(coordinate_type{is...}+m_offsets,m_byte_strides)); }
 
         template<typename IndexContainer>
         void pack(T* buffer, const IndexContainer& c, void* arg)
         {
-            serialization<Device,dimension,layout_map>::pack(buffer, c, m_data, m_extents, m_offsets, m_strides, arg);
+            //serialization<Device,dimension,layout_map>::pack(buffer, c, m_data, m_extents, m_offsets, m_strides, arg);
+            serialization<Device,dimension,layout_map>::pack(buffer, c, m_data, m_byte_strides, m_offsets, arg);
         }
 
         template<typename IndexContainer>
         void unpack(const T* buffer, const IndexContainer& c, void* arg)
         {
-            serialization<Device,dimension,layout_map>::unpack(buffer, c, m_data, m_extents, m_offsets, m_strides, arg);
+            //serialization<Device,dimension,layout_map>::unpack(buffer, c, m_data, m_extents, m_offsets, m_strides, arg);
+            serialization<Device,dimension,layout_map>::unpack(buffer, c, m_data, m_byte_strides, m_offsets, arg);
         }
     };
 
