c9bfe1ad5fd53da2902faeca07fc600aa4afd0a5
bettiolm <bettiolm@cscs.ch>
2019-07-25 19:00:26 +0200

    added allToAll and alToAllv wrappers

--- include/protocol/setup.hpp
+++ include/protocol/setup.hpp
@@ -14,6 +14,7 @@
 #include "./communicator_base.hpp"
 #include <boost/mpi/communicator.hpp>
 #include <vector>
+#include <cassert>
 
 namespace gridtools {
 
@@ -115,7 +116,39 @@ namespace gridtools {
                     m_comm,
                     &h.m_requests[0]));
                 return {std::move(res), std::move(h)};
-            } 
+            }
+
+            /** @brief just a helper function to be used when send/recv counts can be deduced*/
+            template<typename T>
+            void allToAll(const std::vector<T>& send_buf, std::vector<T>& recv_buf) const
+            {
+                std::size_t comm_size = m_comm.size();
+                assert(send_buf.size() % comm_size == 0);
+                assert(recv_buf.size() % comm_size == 0);
+                size_t send_count = send_buf.size() / comm_size * sizeof(T);
+                size_t recv_count = recv_buf.size() / comm_size * sizeof(T);
+                BOOST_MPI_CHECK_RESULT(
+                    MPI_Alltoall,
+                    (send_buf.data(), send_count, MPI_BYTE, recv_buf.data(), recv_count, MPI_BYTE, m_comm));
+            }
+
+            template<typename T>
+            void allToAllv(const T* send_buf, const int* send_counts, const int* send_displs,
+                           T* recv_buf, const int* recv_counts, const int* recv_displs) const
+            {
+                std::size_t comm_size = m_comm.size();
+                std::vector<int> send_counts_b(comm_size), send_displs_b(comm_size), recv_counts_b(comm_size), recv_displs_b(comm_size);
+                for (auto i=0; i<comm_size; ++i) send_counts_b[i] = send_counts[i] * sizeof(T);
+                for (auto i=0; i<comm_size; ++i) send_displs_b[i] = send_displs[i] * sizeof(T);
+                for (auto i=0; i<comm_size; ++i) recv_counts_b[i] = recv_counts[i] * sizeof(T);
+                for (auto i=0; i<comm_size; ++i) recv_displs_b[i] = recv_displs[i] * sizeof(T);
+                BOOST_MPI_CHECK_RESULT(
+                    MPI_Alltoallv,
+                    (send_buf, &send_counts_b[0], &send_displs_b[0], MPI_BYTE,
+                     recv_buf, &recv_counts_b[0], &recv_displs_b[0], MPI_BYTE,
+                     m_comm)
+                );
+            }
 
         private:
             boost::mpi::communicator m_comm;
