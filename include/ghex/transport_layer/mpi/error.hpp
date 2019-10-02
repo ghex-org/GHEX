08dfdaf2b19e7e170f1b3cfedff1b863a5b17cf8
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2019-10-02 16:34:45 +0200

    reinstated error

--- include/ghex/transport_layer/mpi/error.hpp
+++ include/ghex/transport_layer/mpi/error.hpp
@@ -15,13 +15,13 @@
 #include <stdexcept>
 #include <mpi.h>
 
-//#ifdef NDEBUG
-//    #define GHEX_CHECK_MPI_RESULT(x) x;
-//#else
+#ifdef NDEBUG
+    #define GHEX_CHECK_MPI_RESULT(x) x;
+#else
     #define GHEX_CHECK_MPI_RESULT(x) \
     if (x != MPI_SUCCESS)           \
         throw std::runtime_error("GHEX Error: MPI Call failed " + std::string(#x) + " in " + std::string(__FILE__) + ":" + std::to_string(__LINE__));
-//#endif
+#endif
 
 #endif /* INCLUDED_GHEX_TL_MPI_ERROR_HPP */
 
