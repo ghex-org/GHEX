d8ea2d73d015e159110d044f10f4a131357e031e
boeschf <48126478+boeschf@users.noreply.github.com>
2019-12-17 10:34:33 +0100

    Update include/ghex/pattern.hpp
    Co-Authored-By: Mauro Bianco <mbianco@cscs.ch>

--- include/ghex/pattern.hpp
+++ include/ghex/pattern.hpp
@@ -12,7 +12,6 @@
 #define INCLUDED_GHEX_PATTERN_HPP
 
 //#include "./transport_layer/mpi/setup.hpp"
-//#include "./transport_layer/mpi/communicator.hpp"
 #include "./buffer_info.hpp"
 #include "./transport_layer/context.hpp"
 
@@ -108,4 +107,3 @@ namespace gridtools {
 } // namespace gridtools
 
 #endif /* INCLUDED_GHEX_PATTERN_HPP */
-
