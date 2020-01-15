4723db43995d906f8b2239ee918c4b66c82b02ef
Marcin Krotkiewski <marcink@usit.uio.no>
2020-01-15 15:21:38 +0100

    fix test_communicator, works now for multiple threads

--- tests/bindings/test_communicator.f90
+++ tests/bindings/test_communicator.f90
@@ -13,7 +13,8 @@ PROGRAM test_context
   integer :: nthreads = 0, thrid
   integer :: np = 0
   type(ghex_context) :: context
-  type(ghex_communicator), dimension(:), pointer :: comm
+  type(ghex_communicator), dimension(:), pointer :: communicators
+  type(ghex_communicator) :: comm
 
   !$omp parallel shared(nthreads)
   nthreads = omp_get_num_threads()
@@ -24,24 +25,38 @@ PROGRAM test_context
   ! create a context object
   context = context_new(nthreads, mpi_comm_world);
 
-  ! make a communicator - per-thread
+  ! make per-thread communicators
   !$omp parallel private(thrid, comm, np)
 
+  ! make thread id 1-based
+  thrid = omp_get_thread_num()+1
+  print *, "thread ", thrid
+
+  ! initialize shared datastructures
   !$omp master
-  allocate(comm(nthreads))
+  allocate(communicators(nthreads))  
+  !$omp end master
+  !$omp barrier
+
+  ! allocate a communicator per thread and store in a shared array
+  communicators(thrid) = context_get_communicator(context)
+  comm = communicators(thrid)
+
+  ! do some work
+  np = comm_progress(comm)
+
+  ! cleanup per-thread
+  call comm_delete(communicators(thrid))
+
+  ! cleanup shared
+  !$omp barrier
+  !$omp master
+  deallocate(communicators)
   !$omp end master
 
-  !$omp barrier
-  thrid = omp_get_thread_num()
-  ! comm(thrid) => context_get_communicator(context)
-  ! np = comm_progress(comm)
-
-  ! doesn't really have to explicitly destruct comm:
-  ! it is done automatically, when comm is out of scope
-  ! call comm_delete(comm)
   !$omp end parallel
 
   ! delete the ghex context
-  call context_delete(context)
+  call context_delete(context)  
 
 END PROGRAM test_context
