c858a06f464c710348482dc568c59df0d81c7d87
Marcin Krotkiewski <marcink@usit.uio.no>
2020-01-21 14:20:59 +0100

    reset m_completed when marking a request_cb as completed

--- include/ghex/transport_layer/ucx/request.hpp
+++ include/ghex/transport_layer/ucx/request.hpp
@@ -242,6 +242,7 @@ namespace gridtools{
                         if (*m_completed)
                         {
                             m_req = nullptr;
+                            m_completed.reset();
                             return true;
                         }
                         return false;
@@ -249,6 +250,8 @@ namespace gridtools{
 
                     bool cancel()
                     {
+                        // TODO: fix a race. we can only call critical through m_req, but it can be
+                        // set to NULL between when we check below, and when we call the critical region.
                         if (!m_req) return true;
 
                         // TODO at this time, send requests cannot be canceled
@@ -282,6 +285,7 @@ namespace gridtools{
 				    ucp_request_cancel(worker, ucx_ptr);
 				}
 				m_req = nullptr;
+                                m_completed.reset();
 				return true;
 			    }
 			);
