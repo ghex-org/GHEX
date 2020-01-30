91396b443839c2b127e0de718f96c4c364cd9cda
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2020-01-30 16:22:55 +0100

    test for overlapping comms with same tags, but ordered calls

--- tests/transport/test_context.cpp
+++ tests/transport/test_context.cpp
@@ -117,3 +117,80 @@ TEST(context, multi) {
         t.join();
 }
 
+TEST(context, multi_ordered) {
+    const int num_threads = 4;
+    auto context_ptr_1 = gridtools::ghex::tl::context_factory<transport,threading>::create(num_threads, MPI_COMM_WORLD);
+    auto& context_1 = *context_ptr_1;
+
+    auto func = [&context_1]() {
+        auto token_1 = context_1.get_token();
+        auto comm_1 = context_1.get_communicator(token_1);
+
+        auto msg_1 = comm_1.make_message(size*sizeof(int));
+        auto msg_2 = comm_1.make_message(size*sizeof(int));
+
+        if (comm_1.rank() == 0) {
+            const int payload_offset = 1+token_1.id();
+            for (unsigned int i=0; i<size; ++i)
+                *reinterpret_cast<int*>(msg_1.data()+i*sizeof(int)) = i+payload_offset;
+        }
+        if (comm_1.rank() == 0) {
+            const int payload_offset = 2+token_1.id();
+            for (unsigned int i=0; i<size; ++i)
+                *reinterpret_cast<int*>(msg_2.data()+i*sizeof(int)) = i+payload_offset;
+        }
+
+        comm_1.barrier();
+
+        // ordered sends/recvs with same tag should arrive in order
+
+        future fut_1;
+        int counter_1 = 0;
+        if (comm_1.rank() == 0) {
+            for (rank_type i=1; i<comm_1.size(); ++i)
+                comm_1.send(msg_1, i, token_1.id(), [&counter_1](msg_type, rank_type, tag_type) { ++counter_1; });
+        }
+        else {
+            fut_1 = comm_1.recv(msg_1, 0, token_1.id());
+        }
+        future fut_2;
+        int counter_2 = 0;
+        if (comm_1.rank() == 0) {
+            for (rank_type i=1; i<comm_1.size(); ++i)
+                comm_1.send(msg_2, i, token_1.id(), [&counter_2](msg_type, rank_type, tag_type) { ++counter_2; });
+        }
+        else {
+            fut_2 = comm_1.recv(msg_2, 0, token_1.id());
+        }
+
+
+        if (comm_1.rank() == 0)
+            while(counter_1 != comm_1.size()-1) { comm_1.progress(); }
+        if (comm_1.rank() == 0)
+            while(counter_2 != comm_1.size()-1) { comm_1.progress(); }
+
+        if (comm_1.rank() != 0)
+            fut_1.wait();
+        if (comm_1.rank() != 0)
+            fut_2.wait();
+        
+        // check message
+        if (comm_1.rank() != 0) {
+            const int payload_offset = 1+token_1.id();
+            for (unsigned int i=0; i<size; ++i)
+                EXPECT_TRUE(*reinterpret_cast<int*>(msg_1.data()+i*sizeof(int)) == (int)i+payload_offset);
+        }
+        if (comm_1.rank() != 0) {
+            const int payload_offset = 2+token_1.id();
+            for (unsigned int i=0; i<size; ++i)
+                EXPECT_TRUE(*reinterpret_cast<int*>(msg_2.data()+i*sizeof(int)) == (int)i+payload_offset);
+        }
+    };
+
+    std::vector<std::thread> threads;
+    threads.reserve(num_threads);
+    for (int i=0; i<num_threads; ++i)
+        threads.push_back(std::thread{func});
+    for (auto& t : threads)
+        t.join();
+}
