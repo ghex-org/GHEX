df0a36f47735c9779fba678700c7bc8569d44828
bettiolm <bettiolm@cscs.ch>
2020-02-06 16:04:36 +0100

    fix to unified memory allocator, according to PR comment

--- include/ghex/allocator/unified_memory_allocator.hpp
+++ include/ghex/allocator/unified_memory_allocator.hpp
@@ -11,7 +11,10 @@
 #ifndef INCLUDED_GHEX_ALLOCATOR_UNIFIED_MEMORY_ALLOCATOR_HPP
 #define INCLUDED_GHEX_ALLOCATOR_UNIFIED_MEMORY_ALLOCATOR_HPP
 
+#include <cstdlib>
+#include <new>
 #include <stdexcept>
+#include <limits>
 
 #ifdef __CUDACC__
 #include <cuda_runtime.h>
@@ -21,20 +24,24 @@ namespace gridtools {
     namespace ghex {
         namespace allocator {
 
-            // WARN: some member types and member functions provided in std::allocator<T> are still not provided here;
-            // WARN: fails to compile is inheritance is used (potential issues on vector copy constructor when using different allocators)
             template <class T>
             struct unified_memory_allocator {
 
                 typedef T value_type;
 
+                unified_memory_allocator() = default;
+
+                template <class U>
+                constexpr unified_memory_allocator(const unified_memory_allocator<U>&) noexcept {}
+
                 [[nodiscard]] T* allocate(std::size_t n) {
+                    if (n > std::numeric_limits<std::size_t>::max() / sizeof(T)) throw std::bad_alloc();
                     T* p;
 #ifdef __CUDACC__
                     cudaError_t err = cudaMallocManaged(reinterpret_cast<void**>(&p), n * sizeof(T));
                     if (err != cudaSuccess) throw std::runtime_error("ERROR: failed to allocate GPU memory");
 #else
-                    p = reinterpret_cast<T*>(malloc(n * sizeof(T)));
+                    if (!(p = reinterpret_cast<T*>(std::malloc(n * sizeof(T))))) throw std::bad_alloc();
 #endif
                     return p;
                 }
@@ -47,11 +54,17 @@ namespace gridtools {
                     err = cudaFree(reinterpret_cast<void*>(p));
                     if (err != cudaSuccess) throw std::runtime_error("ERROR: failed to free GPU memory");
 #else
-                    free(reinterpret_cast<void*>(p));
+                    std::free(reinterpret_cast<void*>(p));
 #endif
                 }
 
             };
+
+            template <class T, class U>
+            bool operator==(const unified_memory_allocator<T>&, const unified_memory_allocator<U>&) { return true; }
+
+            template <class T, class U>
+            bool operator!=(const unified_memory_allocator<T>&, const unified_memory_allocator<U>&) { return false; }
 
         } // namespace allocator
     } // namespace ghex
