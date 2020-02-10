3df24f25ce0049e53c54be29a6b0e111cb920fbc
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2020-02-10 16:33:07 +0100

    license

--- include/ghex/transport_layer/util/pmi/pmi.hpp
+++ include/ghex/transport_layer/util/pmi/pmi.hpp
@@ -1,13 +1,13 @@
-/*
+/* 
  * GridTools
- *
- * Copyright (c) 2019, ETH Zurich
+ * 
+ * Copyright (c) 2014-2020, ETH Zurich
  * All rights reserved.
- *
+ * 
  * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
+ * 
  */
-
 #ifndef INCLUDED_GHEX_TL_PMI_HPP
 #define INCLUDED_GHEX_TL_PMI_HPP
 
