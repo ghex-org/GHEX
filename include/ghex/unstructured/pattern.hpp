6ff6d5d9504b964710ed19eea0e0c7d859d67139
bettiolm <bettiolm@cscs.ch>
2020-02-11 15:56:58 +0100

    license updated

--- include/ghex/unstructured/pattern.hpp
+++ include/ghex/unstructured/pattern.hpp
@@ -1,12 +1,12 @@
-/*
+/* 
  * GridTools
- *
- * Copyright (c) 2014-2019, ETH Zurich
+ * 
+ * Copyright (c) 2014-2020, ETH Zurich
  * All rights reserved.
- *
+ * 
  * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
- *
+ * 
  */
 #ifndef INCLUDED_GHEX_UNSTRUCTURED_PATTERN_HPP
 #define INCLUDED_GHEX_UNSTRUCTURED_PATTERN_HPP
