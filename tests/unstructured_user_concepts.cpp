0e4b374b083f4e3c3a774e2fd770149b7a3e8e35
bettiolm <bettiolm@cscs.ch>
2020-03-17 14:05:58 +0100

    small test refactoring

--- tests/unstructured_user_concepts.cpp
+++ tests/unstructured_user_concepts.cpp
@@ -36,27 +36,19 @@ using transport = gridtools::ghex::tl::ucx_tag;
 using threading = gridtools::ghex::threads::std_thread::primitives;
 #endif
 using context_type = gridtools::ghex::tl::context<transport, threading>;
+using domain_id_type = int;
+using global_index_type = int;
+using domain_descriptor_type = gridtools::ghex::unstructured::domain_descriptor<domain_id_type, global_index_type>;
+using halo_generator_type = gridtools::ghex::unstructured::halo_generator<domain_id_type, global_index_type>;
+using vertices_type = domain_descriptor_type::vertices_type;
+using vertices_set_type = domain_descriptor_type::vertices_set_type;
+using adjncy_type = domain_descriptor_type::adjncy_type;
+using map_type = domain_descriptor_type::map_type;
+using it_diff_type = vertices_type::iterator::difference_type;
 
 
-/** @brief Test domain descriptor and halo generator concepts */
-TEST(unstructured_user_concepts, domain_descriptor_and_halos) {
-
-    using domain_id_type = int;
-    using global_index_type = int;
-    using domain_descriptor_type = gridtools::ghex::unstructured::domain_descriptor<domain_id_type, global_index_type>;
-    using halo_generator_type = gridtools::ghex::unstructured::halo_generator<domain_id_type, global_index_type>;
-    using vertices_type = domain_descriptor_type::vertices_type;
-    using vertices_set_type = domain_descriptor_type::vertices_set_type;
-    using adjncy_type = domain_descriptor_type::adjncy_type;
-    using map_type = domain_descriptor_type::map_type;
-    using it_diff_type = vertices_type::iterator::difference_type;
-
-    auto context_ptr = gridtools::ghex::tl::context_factory<transport,threading>::create(1, MPI_COMM_WORLD);
-    auto& context = *context_ptr;
-    int rank = context.rank();
-
-    // Explicit initialization without linking to ParMetis
-    switch (rank) {
+map_type init_v_map(const domain_id_type domain_id) {
+    switch (domain_id) {
         case 0: {
             map_type v_map{
                 std::make_pair(global_index_type{0},  adjncy_type{13, 2, 1, 20, 11}),
@@ -64,19 +56,7 @@ TEST(unstructured_user_concepts, domain_descriptor_and_halos) {
                 std::make_pair(global_index_type{5},  adjncy_type{13, 2, 3}),
                 std::make_pair(global_index_type{2},  adjncy_type{0,  5})
             };
-            domain_descriptor_type d{0, v_map};
-            EXPECT_TRUE(d.domain_id() == 0);
-            EXPECT_TRUE(d.inner_size() == 4);
-            EXPECT_TRUE(d.size() == 9);
-            vertices_type inner_vertices{d.vertices().begin(), d.vertices().begin() + static_cast<it_diff_type>(d.inner_size())};
-            vertices_type reference_inner_vertices{0, 13, 5, 2};
-            EXPECT_TRUE(inner_vertices == reference_inner_vertices);
-            halo_generator_type halo_generator{};
-            auto h = halo_generator(d);
-            vertices_set_type halo_vertices_set{h.vertices().begin(), h.vertices().end()};
-            vertices_set_type reference_halo_vertices_set{1, 20, 7, 3, 11};
-            EXPECT_TRUE(halo_vertices_set == reference_halo_vertices_set);
-            break;
+            return v_map;
         }
         case 1: {
             map_type v_map{
@@ -88,19 +68,7 @@ TEST(unstructured_user_concepts, domain_descriptor_and_halos) {
                 std::make_pair(global_index_type{15}, adjncy_type{19, 4,  7,  8}),
                 std::make_pair(global_index_type{8},  adjncy_type{19, 4,  15})
             };
-            domain_descriptor_type d{1, v_map};
-            EXPECT_TRUE(d.domain_id() == 1);
-            EXPECT_TRUE(d.inner_size() == 7);
-            EXPECT_TRUE(d.size() == 11);
-            vertices_type inner_vertices{d.vertices().begin(), d.vertices().begin() + static_cast<it_diff_type>(d.inner_size())};
-            vertices_type reference_inner_vertices{1, 19, 20, 4, 7, 15, 8};
-            EXPECT_TRUE(inner_vertices == reference_inner_vertices);
-            halo_generator_type halo_generator{};
-            auto h = halo_generator(d);
-            vertices_set_type halo_vertices_set{h.vertices().begin(), h.vertices().end()};
-            vertices_set_type reference_halo_vertices_set{0, 13, 16, 9};
-            EXPECT_TRUE(halo_vertices_set == reference_halo_vertices_set);
-            break;
+            return v_map;
         }
         case 2: {
             map_type v_map{
@@ -108,19 +76,7 @@ TEST(unstructured_user_concepts, domain_descriptor_and_halos) {
                 std::make_pair(global_index_type{16}, adjncy_type{1, 18}),
                 std::make_pair(global_index_type{18}, adjncy_type{3, 16})
             };
-            domain_descriptor_type d{2, v_map};
-            EXPECT_TRUE(d.domain_id() == 2);
-            EXPECT_TRUE(d.inner_size() == 3);
-            EXPECT_TRUE(d.size() == 6);
-            vertices_type inner_vertices{d.vertices().begin(), d.vertices().begin() + static_cast<it_diff_type>(d.inner_size())};
-            vertices_type reference_inner_vertices{3, 16, 18};
-            EXPECT_TRUE(inner_vertices == reference_inner_vertices);
-            halo_generator_type halo_generator{};
-            auto h = halo_generator(d);
-            vertices_set_type halo_vertices_set{h.vertices().begin(), h.vertices().end()};
-            vertices_set_type reference_halo_vertices_set{5, 1, 6};
-            EXPECT_TRUE(halo_vertices_set == reference_halo_vertices_set);
-            break;
+            return v_map;
         }
         case 3: {
             map_type v_map{
@@ -131,21 +87,102 @@ TEST(unstructured_user_concepts, domain_descriptor_and_halos) {
                 std::make_pair(global_index_type{12}, adjncy_type{11, 9}),
                 std::make_pair(global_index_type{9},  adjncy_type{4, 6, 10, 12})
             };
-            domain_descriptor_type d{3, v_map};
-            EXPECT_TRUE(d.domain_id() == 3);
+            return v_map;
+        }
+        default: {
+            map_type v_map{};
+            return v_map;
+        }
+    }
+}
+
+
+void check_domain(const domain_descriptor_type& d) {
+    auto domain_id = d.domain_id();
+    switch (domain_id) {
+        case 0: {
+            EXPECT_TRUE(d.inner_size() == 4);
+            EXPECT_TRUE(d.size() == 9);
+            vertices_type inner_vertices{d.vertices().begin(), d.vertices().begin() + static_cast<it_diff_type>(d.inner_size())};
+            vertices_type reference_inner_vertices{0, 13, 5, 2};
+            EXPECT_TRUE(inner_vertices == reference_inner_vertices);
+            break;
+        }
+        case 1: {
+            EXPECT_TRUE(d.inner_size() == 7);
+            EXPECT_TRUE(d.size() == 11);
+            vertices_type inner_vertices{d.vertices().begin(), d.vertices().begin() + static_cast<it_diff_type>(d.inner_size())};
+            vertices_type reference_inner_vertices{1, 19, 20, 4, 7, 15, 8};
+            EXPECT_TRUE(inner_vertices == reference_inner_vertices);
+            break;
+        }
+        case 2: {
+            EXPECT_TRUE(d.inner_size() == 3);
+            EXPECT_TRUE(d.size() == 6);
+            vertices_type inner_vertices{d.vertices().begin(), d.vertices().begin() + static_cast<it_diff_type>(d.inner_size())};
+            vertices_type reference_inner_vertices{3, 16, 18};
+            EXPECT_TRUE(inner_vertices == reference_inner_vertices);
+            break;
+        }
+        case 3: {
             EXPECT_TRUE(d.inner_size() == 6);
             EXPECT_TRUE(d.size() == 9);
             vertices_type inner_vertices{d.vertices().begin(), d.vertices().begin() + static_cast<it_diff_type>(d.inner_size())};
             vertices_type reference_inner_vertices{17, 6, 11, 10, 12, 9};
             EXPECT_TRUE(inner_vertices == reference_inner_vertices);
-            halo_generator_type halo_generator{};
-            auto h = halo_generator(d);
+            break;
+        }
+    }
+}
+
+
+void check_halo_generator(const domain_descriptor_type& d, const halo_generator_type& hg) {
+    auto h = hg(d);
+    switch (d.domain_id()) {
+        case 0: {
+            vertices_set_type halo_vertices_set{h.vertices().begin(), h.vertices().end()};
+            vertices_set_type reference_halo_vertices_set{1, 20, 7, 3, 11};
+            EXPECT_TRUE(halo_vertices_set == reference_halo_vertices_set);
+            break;
+        }
+        case 1: {
+            vertices_set_type halo_vertices_set{h.vertices().begin(), h.vertices().end()};
+            vertices_set_type reference_halo_vertices_set{0, 13, 16, 9};
+            EXPECT_TRUE(halo_vertices_set == reference_halo_vertices_set);
+            break;
+        }
+        case 2: {
+            vertices_set_type halo_vertices_set{h.vertices().begin(), h.vertices().end()};
+            vertices_set_type reference_halo_vertices_set{5, 1, 6};
+            EXPECT_TRUE(halo_vertices_set == reference_halo_vertices_set);
+            break;
+        }
+        case 3: {
             vertices_set_type halo_vertices_set{h.vertices().begin(), h.vertices().end()};
             vertices_set_type reference_halo_vertices_set{0, 4, 3};
             EXPECT_TRUE(halo_vertices_set == reference_halo_vertices_set);
             break;
         }
     }
+}
+
+
+/** @brief Test domain descriptor and halo generator concepts */
+TEST(unstructured_user_concepts, domain_descriptor_and_halos) {
+
+    auto context_ptr = gridtools::ghex::tl::context_factory<transport,threading>::create(1, MPI_COMM_WORLD);
+    auto& context = *context_ptr;
+    int rank = context.rank();
+
+    // domain
+    domain_id_type domain_id{rank}; // 1 domain per rank
+    auto v_map = init_v_map(domain_id);
+    domain_descriptor_type d{domain_id, v_map};
+    check_domain(d);
+
+    // halo_generator
+    halo_generator_type hg{};
+    check_halo_generator(d, hg);
 
 }
 
