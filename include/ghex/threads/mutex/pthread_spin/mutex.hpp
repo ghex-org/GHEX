7e3db8816eb8efad669139e8d75725679a275c17
Mauro Bianco <mbianco@cscs.ch>
2020-04-10 12:06:20 +0200

    changes to enable spinlock on macos

--- include/ghex/threads/mutex/pthread_spin/mutex.hpp
+++ include/ghex/threads/mutex/pthread_spin/mutex.hpp
@@ -1,12 +1,12 @@
-/* 
+/*
  * GridTools
- * 
+ *
  * Copyright (c) 2014-2020, ETH Zurich
  * All rights reserved.
- * 
+ *
  * Please, refer to the LICENSE file in the root directory.
  * SPDX-License-Identifier: BSD-3-Clause
- * 
+ *
  */
 #ifndef INCLUDED_GHEX_THREADS_MUTEX_PTHREAD_SPIN_HPP
 #define INCLUDED_GHEX_THREADS_MUTEX_PTHREAD_SPIN_HPP
@@ -14,6 +14,45 @@
 #include <mutex>
 #include <pthread.h>
 #include <sched.h>
+
+#ifdef _MACOS_GHEX_
+typedef int pthread_spinlock_t;
+
+int pthread_spin_init(pthread_spinlock_t *lock, int pshared) {
+    __asm__ __volatile__ ("" ::: "memory");
+    *lock = 0;
+    return 0;
+}
+
+int pthread_spin_destroy(pthread_spinlock_t *lock) {
+    return 0;
+}
+
+int pthread_spin_lock(pthread_spinlock_t *lock) {
+    while (1) {
+        int i;
+        for (i=0; i < 10000; i++) {
+            if (__sync_bool_compare_and_swap(lock, 0, 1)) {
+                return 0;
+            }
+        }
+        sched_yield();
+    }
+}
+
+int pthread_spin_trylock(pthread_spinlock_t *lock) {
+    if (__sync_bool_compare_and_swap(lock, 0, 1)) {
+        return 0;
+    }
+    return EBUSY;
+}
+
+int pthread_spin_unlock(pthread_spinlock_t *lock) {
+    __asm__ __volatile__ ("" ::: "memory");
+    *lock = 0;
+    return 0;
+}
+#endif
 
 namespace gridtools {
     namespace ghex {
@@ -26,7 +65,7 @@ namespace gridtools {
                     private: // members
                         pthread_spinlock_t m_lock;
                     public:
-                        mutex() noexcept 
+                        mutex() noexcept
                         {
                             pthread_spin_init(&m_lock, PTHREAD_PROCESS_PRIVATE);
                         }
@@ -41,16 +80,16 @@ namespace gridtools {
                         {
                             return (pthread_spin_trylock(&m_lock)==0);
                         }
-                           
+
                         inline void lock() noexcept
                         {
                             while (!try_lock()) { sched_yield(); }
-                        } 
+                        }
 
                         inline void unlock() noexcept
                         {
                             pthread_spin_unlock(&m_lock);
-                        } 
+                        }
                     };
 
                     using lock_guard = std::lock_guard<mutex>;
