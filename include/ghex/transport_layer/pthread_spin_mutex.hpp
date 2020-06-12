a5e1e8ac35dd84c1342f0ad7a9d59f11a02812db
Mauro Bianco <mbianco@cscs.ch>
2020-06-12 09:31:59 -0400

    added license

--- include/ghex/transport_layer/pthread_spin_mutex.hpp
+++ include/ghex/transport_layer/pthread_spin_mutex.hpp
@@ -1,3 +1,14 @@
+/*
+ * GridTools
+ *
+ * Copyright (c) 2014-2020, ETH Zurich
+ * All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
 #ifndef GHEX_PTHREAD_SPIN_MUTEX_HPP
 #define GHEX_PTHREAD_SPIN_MUTEX_HPP
 
