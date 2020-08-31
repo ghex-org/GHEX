8695755509a0a99783a3c4d7a1b83aec77b03920
bettiolm <bettiolm@cscs.ch>
2020-08-31 11:43:43 +0200

    Setting up initial data

--- benchmarks/unstructured_parmetis.cpp
+++ benchmarks/unstructured_parmetis.cpp
@@ -9,13 +9,12 @@
  * 
  */
 
-// TO DO: headers for test code start here
 
+#include <cstdint>
+#include <fstream>
 #include <vector>
 
-// TO DO: headers for test code end here
-
-#include <fstream>
+#include <mpi.h>
 
 #include <gtest/gtest.h>
 
@@ -43,34 +42,73 @@ using threading = gridtools::ghex::threads::std_thread::primitives;
 
 template<typename T>
 char* as_bytes(T& i) {
-    return reinterpret_cast<char*>(&i);
+    return reinterpret_cast<char*>(&i); // TO DO: really, is it safe?
+}
+
+
+template<typename C>
+void debug_print(const C& c) {
+    std::cout << "Size = " << c.size() << "; elements = [ ";
+    for (const auto& elem : c) { std::cout << elem << " "; }
+    std::cout << "]\n";
 }
 
 
 /** @brief Unstructured exchange benchmark (in-place receive against buffered receive)*/
 TEST(unstructured_parmetis, receive_type) {
 
+    // type definitions
+    using data_int_type = int64_t;
+
+    // MPI setup
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // input data
+    std::ifstream ap_fs("Ap.out", std::ios_base::binary);
+    ap_fs.seekg(0, std::ios_base::end); // go to the end
+
+    // TO DO: debug info, please remove
+    if (rank == 0) {
+        std::cout << "number of int64_t read: " << ap_fs.tellg() / sizeof(data_int_type) << "\n";
+    } // output is 817 (CSR format provides always the two endpoints, first included, second excluded)
+
+    data_int_type all_num_vertices = ap_fs.tellg() / sizeof(data_int_type) - 1;
+    auto pos = all_num_vertices / size * sizeof(data_int_type) * rank;
+    ap_fs.seekg(pos); // rewind to begin of section, according to rank (remainder is handled entirely by last rank, TO DO: not optimal)
+    if (rank == (size - 1)) { // last rank reads until eof
+        std::vector<data_int_type> ap{};
+        for (data_int_type b; ap_fs.read(as_bytes(b), sizeof(b)); ) {
+            ap.push_back(b);
+        }
+    } else { // all other ranks read until end of their section // HERE
+        data_int_type section_size = all_num_vertices / size + 1; // (CSR format provides always the two endpoints, first included, second excluded)
+        std::vector<data_int_type> ap(section_size);
+    }
+
+    // std::ifstream ai_fs("Ai.out", std::ios_base::binary);
+
+    // std::vector<data_int_type> ai{};
+    // for (data_int_type i; ai_fs.read(as_bytes(i), sizeof(i));) { ai.push_back(i); } // TO DO: set bounds according to rank
+
+    /* // TO DO: debug info, please remove
+    std::cout << "Ap:\n";
+    debug_print(ap);
+    std::cout << "\n";
+    std::cout << "Ai:\n";
+    debug_print(ai); */
+
+    // prepare parmetis routine arrays
+
+    // call parmetis routine
+
+    // repartition output according to parmetis labelling
+
+    // GHEX context
     auto context_ptr = gridtools::ghex::tl::context_factory<transport,threading>::create(1, MPI_COMM_WORLD);
     auto& context = *context_ptr;
-    int rank = context.rank();
 
-    std::ifstream ap_fs("Ap.out", std::ios_base::binary);
-    std::ifstream ai_fs("Ai.out", std::ios_base::binary);
-
-    // TO DO: sample code starts here (to be removed)
-
-    std::vector<long long unsigned int> ap{};
-    std::vector<long long unsigned int> ai{};
-
-    for (long long unsigned int i; ap_fs.read(as_bytes(i), sizeof(i));) {
-        ap.push_back(i);
-    }
-
-    for (long long unsigned int i; ai_fs.read(as_bytes(i), sizeof(i));) {
-        ai.push_back(i);
-    }
-
-    // TO DO: sample code ends here (to be removed)
-
+    // GHEX user concepts
 
 }
