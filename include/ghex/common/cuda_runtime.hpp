8fdedf83653610166389131fe04143efdf38672d
bettiolm <bettiolm@cscs.ch>
2021-04-08 12:56:43 +0200

    split gpu header - definitions

--- include/ghex/common/cuda_runtime.hpp
+++ include/ghex/common/cuda_runtime.hpp
@@ -11,12 +11,6 @@
 #ifndef INCLUDED_GHEX_COMMON_CUDA_RUNTIME_HPP
 #define INCLUDED_GHEX_COMMON_CUDA_RUNTIME_HPP
 
-// if necessary, this define might be moved to a defs.hpp,
-// or the GridTools one might be used instead
-#if (defined(__CUDACC__) || defined(__HIP__))
-#define GHEX_CUDACC
-#endif
-
 #include <gridtools/common/cuda_runtime.hpp>
 
 // TO DO: add missing cuda -> hip translations when necessary
