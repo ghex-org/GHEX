38902033f638e9d28b49dff8a8821be8127c8221
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2021-08-31 14:49:03 +0200

    gridtools glue

---
+++ include2/ghex/glue/gridtools/field.hpp
@@ -0,0 +1,82 @@
+/* 
+ * GridTools
+ * 
+ * Copyright (c) 2014-2021, ETH Zurich
+ * All rights reserved.
+ * 
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * 
+ */
+#pragma once
+
+#include <ghex/config.hpp>
+#include <ghex/arch_traits.hpp>
+#include <ghex/structured/regular/field_descriptor.hpp>
+#include <ghex/glue/gridtools/processor_grid.hpp>
+#include <gridtools/storage/data_store.hpp>
+#include <gridtools/meta/list_to_iseq.hpp>
+
+namespace ghex
+{
+namespace _impl
+{
+//template<typename Halo, typename S, typename I, I... Is>
+//array<S, sizeof...(Is)> get_begin(std::integer_sequence<I, Is...>)
+//{
+//    return {Halo::template at<Is>()...};
+//}
+
+template<typename Seq>
+struct get_layout_map;
+
+template<template<class T, T...> class Seq, typename I, I... Is>
+struct get_layout_map<Seq<I, Is...>>
+{
+    using type = gridtools::layout_map<Is...>;
+};
+
+template<typename Layout>
+struct get_unmasked_layout_map;
+
+template<int... Args>
+struct get_unmasked_layout_map<gridtools::layout_map<Args...>>
+{
+    using args = gridtools::meta::list<std::integral_constant<int, Args>...>;
+    using unmasked_args =
+        gridtools::meta::filter<gridtools::_impl::_layout_map::not_negative, args>;
+    using integer_seq = gridtools::meta::list_to_iseq<unmasked_args>;
+    using type = typename get_layout_map<integer_seq>::type;
+};
+
+} // namespace _impl
+
+template<typename Arch, typename DomainDescriptor, typename DataStore>
+auto
+wrap_gt_field(const DomainDescriptor& dom, const std::shared_ptr<DataStore>& ds,
+    const std::array<int, DataStore::ndims>& origin, int device_id = 0)
+{
+    using value_t = typename DataStore::data_t;
+    using layout_t = typename DataStore::layout_t;
+    using unmasked_layout_t = typename _impl::get_unmasked_layout_map<layout_t>::type;
+    using uint_t = decltype(layout_t::masked_length);
+    using dimension = std::integral_constant<uint_t, layout_t::masked_length>;
+    using field_desc_t =
+        structured::regular::field_descriptor<value_t, Arch, DomainDescriptor, unmasked_layout_t>;
+
+    auto strides = ds->strides();
+    for (unsigned int i = 0u; i < dimension::value; ++i) strides[i] *= sizeof(value_t);
+
+    return field_desc_t(
+        dom, ds->get_target_ptr(), origin, ds->lengths(), strides, 1, false, device_id);
+}
+
+template<typename Arch, typename DataStore, typename Origin>
+auto
+wrap_gt_field(const gt_grid& grid, DataStore&& ds, Origin&& origin, int device_id = 0)
+{
+    return wrap_gt_field<Arch>(
+        grid.m_domains[0], std::forward<DataStore>(ds), std::forward<Origin>(origin), device_id);
+}
+
+} // namespace ghex
---
+++ include2/ghex/glue/gridtools/gt_glue.hpp
@@ -0,0 +1,15 @@
+/* 
+ * GridTools
+ * 
+ * Copyright (c) 2014-2021, ETH Zurich
+ * All rights reserved.
+ * 
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * 
+ */
+#pragma once
+
+#include <ghex/glue/gridtools/processor_grid.hpp>
+#include <ghex/glue/gridtools/make_gt_pattern.hpp>
+#include <ghex/glue/gridtools/field.hpp>
