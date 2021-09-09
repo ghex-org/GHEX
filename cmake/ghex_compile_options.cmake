3f9eb26869aefc89f62ecb2a3c1334a5dbfa77d3
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2021-09-09 11:33:42 +0200

    avoid clang warning, link to hip

--- cmake/ghex_compile_options.cmake
+++ cmake/ghex_compile_options.cmake
@@ -1,5 +1,6 @@
 
 set(c_cxx_lang "$<COMPILE_LANGUAGE:C,CXX>")
+set(c_cxx_lang_clang "$<COMPILE_LANG_AND_ID:CXX,Clang>")
 set(cuda_lang "$<COMPILE_LANGUAGE:CUDA>")
 set(fortran_lang "$<COMPILE_LANGUAGE:Fortran>")
 set(fortran_lang_gnu "$<COMPILE_LANG_AND_ID:Fortran,GNU>")
@@ -8,6 +9,7 @@ function(ghex_target_compile_options target)
     target_compile_options(${target} PRIVATE
     # flags for CXX builds
     $<${c_cxx_lang}:$<BUILD_INTERFACE:-Wall -Wextra -Wpedantic -Wno-unknown-pragmas>>
+    $<${c_cxx_lang_clang}:$<BUILD_INTERFACE:-Wno-c++17-extensions>>
     # flags for CUDA builds
     $<${cuda_lang}:$<BUILD_INTERFACE:-Xcompiler=-Wall -Wextra -Wno-unknown-pragmas --default-stream per-thread>>
     # flags for Fortran builds
