9dd8e43180ed0773e7e53476f7640feeb450f4e3
bettiolm <bettiolm@cscs.ch>
2021-09-15 20:45:26 +0200

    second cpu test ported

--- tests/atlas_integration_comm_2.cpp
+++ tests/atlas_integration_comm_2.cpp
@@ -175,7 +175,7 @@ TEST(atlas_integration, halo_exchange) {
         auto GHEX_field_1_gpu_data = GHEX_field_1_gpu.const_host_view();
         for (auto node = 0; node < fs_nodes.nb_nodes(); ++node) {
             for (auto level = 0; level < fs_nodes.levels(); ++level) {
-                EXPECT_TRUE(GHEX_field_1_gpu_data(node, level, 0) == atlas_field_1_data(node, level));
+                EXPECT_TRUE(GHEX_field_1_gpu_data(node, level, 0) == atlas_field_1_data(node, level)); // TO DO: hard-coded 3d view. Should be more flexible
             }
         }
     }
@@ -185,14 +185,15 @@ TEST(atlas_integration, halo_exchange) {
 }
 
 
-/* TO DO: test temporarily disabled, enable again once first is ok
 TEST(atlas_integration, halo_exchange_multiple_patterns) {
 
     using domain_id_t = int;
     using domain_descriptor_t = gridtools::ghex::atlas_domain_descriptor<domain_id_t>;
     using grid_type = gridtools::ghex::unstructured::grid;
-    using cpu_int_data_descriptor_t = gridtools::ghex::atlas_data_descriptor<gridtools::ghex::cpu, domain_id_t, int>;
-    using cpu_double_data_descriptor_t = gridtools::ghex::atlas_data_descriptor<gridtools::ghex::cpu, domain_id_t, double>;
+    using storage_traits_cpu = gridtools::storage::cpu_kfirst;
+    using function_space_t = atlas::functionspace::NodeColumns;
+    using cpu_int_data_descriptor_t = gridtools::ghex::atlas_data_descriptor<gridtools::ghex::cpu, domain_id_t, int, storage_traits_cpu, function_space_t>;
+    using cpu_double_data_descriptor_t = gridtools::ghex::atlas_data_descriptor<gridtools::ghex::cpu, domain_id_t, double, storage_traits_cpu, function_space_t>;
 
     auto context_ptr = gridtools::ghex::tl::context_factory<transport>::create(MPI_COMM_WORLD);
     auto& context = *context_ptr;
@@ -222,7 +223,7 @@ TEST(atlas_integration, halo_exchange_multiple_patterns) {
     // Generate functionspace associated to the mesh with halo size = 2
     atlas::functionspace::NodeColumns fs_nodes_2(mesh, atlas::option::levels(nb_levels) | atlas::option::halo(2));
 
-    // Instantiate domain descriptor (halo size = 1)
+    // Instantiate domain descriptor (halo size = 2)
     std::vector<domain_descriptor_t> local_domains_2{};
     domain_descriptor_t d_2{rank,
                             mesh.nodes().partition(),
@@ -244,35 +245,42 @@ TEST(atlas_integration, halo_exchange_multiple_patterns) {
     auto co = gridtools::ghex::make_communication_object<decltype(patterns_1)>(context.get_communicator());
 
     // Fields creation and initialization
-    atlas::FieldSet fields_1, fields_2;
-    fields_1.add(fs_nodes_1.createField<int>(atlas::option::name("serial_field_1")));
-    fields_1.add(fs_nodes_1.createField<int>(atlas::option::name("multi_field_1")));
-    fields_2.add(fs_nodes_2.createField<double>(atlas::option::name("serial_field_2")));
-    fields_2.add(fs_nodes_2.createField<double>(atlas::option::name("multi_field_2")));
-    auto serial_field_1_data = atlas::array::make_view<int, 2>(fields_1["serial_field_1"]);
-    auto multi_field_1_data = atlas::array::make_view<int, 2>(fields_1["multi_field_1"]);
-    auto serial_field_2_data = atlas::array::make_view<double, 2>(fields_2["serial_field_2"]);
-    auto multi_field_2_data = atlas::array::make_view<double, 2>(fields_2["multi_field_2"]);
-    for (auto node = 0; node < fs_nodes_1.nb_nodes(); ++node) {
-        for (auto level = 0; level < fs_nodes_1.levels(); ++level) {
-            auto value = (rank << 15) + (node << 7) + level;
-            serial_field_1_data(node, level) = value;
-            multi_field_1_data(node, level) = value;
+    auto serial_field_1 = gridtools::ghex::atlas::make_field<int, storage_traits_cpu>(fs_nodes_1, 1); // 1 component / scalar field
+    auto multi_field_1 = gridtools::ghex::atlas::make_field<int, storage_traits_cpu>(fs_nodes_1, 1); // 1 component / scalar field
+    auto serial_field_2 = gridtools::ghex::atlas::make_field<double, storage_traits_cpu>(fs_nodes_2, 1); // 1 component / scalar field
+    auto multi_field_2 = gridtools::ghex::atlas::make_field<double, storage_traits_cpu>(fs_nodes_2, 1); // 1 component / scalar field
+    {
+        auto serial_field_1_data = serial_field_1.host_view();
+        auto multi_field_1_data = multi_field_1.host_view();
+        auto serial_field_2_data = serial_field_2.host_view();
+        auto multi_field_2_data = multi_field_2.host_view();
+        for (auto node = 0; node < fs_nodes_1.nb_nodes(); ++node) {
+            for (auto level = 0; level < fs_nodes_1.levels(); ++level) {
+                auto value = (rank << 15) + (node << 7) + level;
+                serial_field_1_data(node, level, 0) = value; // TO DO: hard-coded 3d view. Should be more flexible
+                multi_field_1_data(node, level, 0) = value; // TO DO: hard-coded 3d view. Should be more flexible
+            }
         }
-    }
-    for (auto node = 0; node < fs_nodes_2.nb_nodes(); ++node) {
-        for (auto level = 0; level < fs_nodes_2.levels(); ++level) {
-            auto value = ((rank << 15) + (node << 7) + level) * 0.5;
-            serial_field_2_data(node, level) = value;
-            multi_field_2_data(node, level) = value;
+        for (auto node = 0; node < fs_nodes_2.nb_nodes(); ++node) {
+            for (auto level = 0; level < fs_nodes_2.levels(); ++level) {
+                auto value = ((rank << 15) + (node << 7) + level) * 0.5;
+                serial_field_2_data(node, level, 0) = value; // TO DO: hard-coded 3d view. Should be more flexible
+                multi_field_2_data(node, level, 0) = value; // TO DO: hard-coded 3d view. Should be more flexible
+            }
         }
     }
 
+    // Target views
+    auto serial_field_1_target_data = serial_field_1.target_view();
+    auto multi_field_1_target_data = multi_field_1.target_view();
+    auto serial_field_2_target_data = serial_field_2.target_view();
+    auto multi_field_2_target_data = multi_field_2.target_view();
+
     // Instantiate data descriptors
-    cpu_int_data_descriptor_t serial_data_1{local_domains_1.front(), fields_1["serial_field_1"]};
-    cpu_int_data_descriptor_t multi_data_1{local_domains_1.front(), fields_1["multi_field_1"]};
-    cpu_double_data_descriptor_t serial_data_2{local_domains_2.front(), fields_2["serial_field_2"]};
-    cpu_double_data_descriptor_t multi_data_2{local_domains_2.front(), fields_2["multi_field_2"]};
+    cpu_int_data_descriptor_t serial_data_1{local_domains_1.front(), serial_field_1_target_data, serial_field_1.components()};
+    cpu_int_data_descriptor_t multi_data_1{local_domains_1.front(), multi_field_1_target_data, multi_field_1.components()};
+    cpu_double_data_descriptor_t serial_data_2{local_domains_2.front(), serial_field_2_target_data, serial_field_2.components()};
+    cpu_double_data_descriptor_t multi_data_2{local_domains_2.front(), multi_field_2_target_data, multi_field_2.components()};
 
     // Serial halo exchange
     auto h_s1 = co.exchange(patterns_1(serial_data_1));
@@ -285,16 +293,24 @@ TEST(atlas_integration, halo_exchange_multiple_patterns) {
     h_m.wait();
 
     // Test for correctness
-    for (auto node = 0; node < fs_nodes_1.nb_nodes(); ++node) {
-        for (auto level = 0; level < fs_nodes_1.levels(); ++level) {
-            EXPECT_TRUE(serial_field_1_data(node, level) == multi_field_1_data(node, level));
+    {
+        auto serial_field_1_data = serial_field_1.const_host_view();
+        auto multi_field_1_data = multi_field_1.const_host_view();
+        auto serial_field_2_data = serial_field_2.const_host_view();
+        auto multi_field_2_data = multi_field_2.const_host_view();
+        for (auto node = 0; node < fs_nodes_1.nb_nodes(); ++node) {
+            for (auto level = 0; level < fs_nodes_1.levels(); ++level) {
+                EXPECT_TRUE(serial_field_1_data(node, level, 0) == multi_field_1_data(node, level, 0)); // TO DO: hard-coded 3d view. Should be more flexible
+            }
+        }
+        for (auto node = 0; node < fs_nodes_2.nb_nodes(); ++node) {
+            for (auto level = 0; level < fs_nodes_2.levels(); ++level) {
+                EXPECT_TRUE(serial_field_2_data(node, level, 0) == multi_field_2_data(node, level, 0)); // TO DO: hard-coded 3d view. Should be more flexible
+            }
         }
     }
-    for (auto node = 0; node < fs_nodes_2.nb_nodes(); ++node) {
-        for (auto level = 0; level < fs_nodes_2.levels(); ++level) {
-            EXPECT_TRUE(serial_field_2_data(node, level) == multi_field_2_data(node, level));
-        }
-    }
+
+    // HERE
 
 #ifdef GHEX_CUDACC
     // Additional data descriptor types for GPU
@@ -347,4 +363,3 @@ TEST(atlas_integration, halo_exchange_multiple_patterns) {
 #endif
 
 }
-*/
