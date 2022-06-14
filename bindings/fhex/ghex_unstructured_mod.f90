edc56f506b77aec0b521a12eea5467a4b6c5dbcd
bettiolm <bettiolm@cscs.ch>
2022-06-14 11:02:27 +0200

    cleaning

--- bindings/fhex/ghex_unstructured_mod.f90
+++ bindings/fhex/ghex_unstructured_mod.f90
@@ -1,5 +1,4 @@
 MODULE ghex_unstructured_mod
-
     !
     ! GridTools
     !
@@ -32,6 +31,16 @@ MODULE ghex_unstructured_mod
     type, bind(c) :: ghex_unstruct_pattern
         type(c_ptr) :: ptr = c_null_ptr
     end type ghex_unstruct_pattern
+
+    ! field descriptor
+    type, bind(c) :: ghex_unstruct_field_desc
+        type(c_ptr) :: domain = c_null_ptr
+        type(c_ptr) :: field = c_null_ptr ! OK
+        integer(c_int) :: field_size = 0 ! From HERE (included)
+        integer(c_int) :: device_id = GhexDeviceUnknown
+    end type ghex_unstruct_field
+
+    ! To HERE
 
     ! ---------------------
     ! --- module C interfaces
@@ -81,116 +90,5 @@ CONTAINS
         type(ghex_unstruct_domain_desc), dimension(:), target :: domain_descs
         call ghex_unstruct_pattern_setup_impl(pattern, c_loc(domain_descs), size(domain_descs))
     end subroutine ghex_unstruct_pattern_setup
-
-    ! --- HERE
-
-    ! field descriptor
-    type, bind(c) :: ghex_unstruct_field
-        import ghex_unstruct_domain
-        type(ghex_unstruct_domain) :: domain
-        type(c_ptr) :: data = c_null_ptr
-        integer(c_int) :: device_id = GhexDeviceUnknown
-    end type ghex_unstruct_field
-
-    ! unstructured grid communication object
-    type, bind(c) :: ghex_unstruct_communication_object
-        type(c_ptr) :: ptr = c_null_ptr
-    end type ghex_unstruct_communication_object
-
-    ! definition of the exchange, including physical fields and the communication pattern
-    type, bind(c) :: ghex_unstruct_exchange_descriptor
-        type(c_ptr) :: ptr = c_null_ptr
-    end type ghex_unstruct_exchange_descriptor
-
-    ! a handle to track a particular communication instance, supports wait()
-    type, bind(c) :: ghex_unstruct_exchange_handle
-        type(c_ptr) :: ptr = c_null_ptr
-    end type ghex_unstruct_exchange_handle
-
-    ! ---------------------
-    ! --- generic ghex interfaces
-    ! ---------------------
-
-    interface ghex_free
-        subroutine ghex_unstruct_exchange_desc_free(exchange_desc) bind(c, name="ghex_obj_free")
-            use iso_c_binding
-            import ghex_unstruct_exchange_descriptor
-            type(ghex_unstruct_exchange_descriptor) :: exchange_desc
-        end subroutine ghex_unstruct_exchange_desc_free
-
-        subroutine ghex_unstruct_exchange_handle_free(exchange_handle) bind(c, name="ghex_obj_free")
-            use iso_c_binding
-            import ghex_unstruct_exchange_handle
-            type(ghex_unstruct_exchange_handle) :: exchange_handle
-        end subroutine ghex_unstruct_exchange_handle_free
-
-        subroutine ghex_unstruct_domain_free(domains_desc) bind(c)
-            use iso_c_binding
-            import ghex_unstruct_domain
-            type(ghex_unstruct_domain) :: domains_desc
-        end subroutine ghex_unstruct_domain_free
-
-        subroutine ghex_unstruct_co_free(co) bind(c, name="ghex_obj_free")
-            use iso_c_binding
-            import ghex_unstruct_communication_object
-            ! reference, not a value - fortran variable is reset to null
-            type(ghex_unstruct_communication_object) :: co
-        end subroutine ghex_unstruct_co_free
-
-        procedure :: ghex_unstruct_field_free
-    end interface ghex_free
-
-    interface ghex_field_init
-        procedure :: ghex_unstruct_field_init
-    end interface ghex_field_init
-
-    interface ghex_co_init
-        subroutine ghex_unstruct_co_init(co) bind(c)
-            import ghex_unstruct_communication_object
-            type(ghex_unstruct_communication_object) :: co
-        end subroutine ghex_unstruct_co_init
-    end interface ghex_co_init
-
-    ! TO DO: TO BE CHANGED (should be ghex_exchange_desc_add_field)
-    ! interface ghex_domain_add_field
-        ! ...
-    ! end interface ghex_domain_add_field
-
-    interface ghex_exchange_desc_new
-        procedure :: ghex_unstruct_exchange_desc_new
-    end interface ghex_exchange_desc_new
-
-    interface ghex_exchange
-        type(ghex_unstruct_exchange_handle) function ghex_unstruct_exchange(co, exchange_desc) bind(c)
-            import ghex_unstruct_exchange_handle, ghex_unstruct_communication_object, ghex_unstruct_exchange_descriptor
-            type(ghex_unstruct_communication_object), value :: co
-            type(ghex_unstruct_exchange_descriptor), value :: exchange_desc
-        end function ghex_unstruct_exchange
-    end interface ghex_exchange
-
-    interface ghex_wait
-        subroutine ghex_unstruct_exchange_handle_wait(exchange_handle) bind(c)
-            use iso_c_binding
-            import ghex_unstruct_exchange_handle
-            type(ghex_unstruct_exchange_handle) :: exchange_handle
-        end subroutine ghex_unstruct_exchange_handle_wait
-    end interface ghex_wait
-
-CONTAINS
-
-    subroutine ghex_unstruct_field_init(field_desc)
-        type(ghex_struct_field) :: field_desc
-        ! TO DO
-    end subroutine ghex_unstruct_field_init
-
-    subroutine ghex_unstruct_field_free(field_desc)
-        type(ghex_unstruct_field) :: field_desc
-        ! TO DO
-    end subroutine ghex_unstruct_field_free
-
-    type(ghex_unstruct_exchange_descriptor) function ghex_unstruct_exchange_desc_new(domains_desc)
-        type(ghex_unstruct_domain), target :: domains_desc
-        ghex_unstruct_exchange_desc_new = ghex_unstruct_exchange_desc_new_wrapped(c_loc(domains_desc), 1);
-    end function ghex_unstruct_exchange_desc_new
 
 END MODULE ghex_unstructured_mod
