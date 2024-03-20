6ca83853d2737b626f52cc502413bb8de44025d1
boeschf <48126478+boeschf@users.noreply.github.com>
2024-03-20 15:27:38 +0100

    move

--- bindings/python/src/ghex/structured/regular.py
+++ bindings/python/src/ghex/structured/regular.py
@@ -9,14 +9,16 @@
 #
 from __future__ import annotations
 from typing import TYPE_CHECKING
-<<<<<<< HEAD
-from ghex.util.cpp_wrapper import CppWrapper, cls_from_cpp_type_spec, dtype_to_cpp, unwrap
+from dataclasses import dataclass
+
 from ghex.pyghex import make_co_regular as _make_co_regular
+from ghex.util.cpp_wrapper import CppWrapper, cls_from_cpp_type_spec, dtype_to_cpp, unwrap
 from ghex.util.architecture import Architecture
+from ghex.structured.cartesian_sets import CartesianSet, ProductSet, union
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
-    from typing import Any
+    from typing import Any, Union
 
     from ghex.structured.index_space import CartesianSet
     from ghex.context import context
@@ -96,3 +98,52 @@ def make_field_descriptor(
 
 def wrap_field(*args):
     return make_field_descriptor(*args)
+
+@dataclass
+class HaloContainer:
+    local: CartesianSet
+    global_: CartesianSet
+
+
+class HaloGenerator(CppWrapper):
+    def __init__(
+        self,
+        glob_domain_indices: ProductSet,
+        halos: tuple[Union[int, tuple[int, int]], ...],
+        periodicity: tuple[bool, ...],
+    ) -> None:
+        assert glob_domain_indices.dim == len(halos)
+        assert glob_domain_indices.dim == len(periodicity)
+
+        # canonicalize integer halos, e.g. turn (h0, (h1, h2), h3) into ((h0, h0), (h1, h2), ...)
+        halos2 = ((halo, halo) if isinstance(halo, int) else halo for halo in halos)
+        flattened_halos = tuple(h for halo in halos2 for h in halo)
+
+        super(HaloGenerator, self).__init__(
+            (
+                "structured__regular__halo_generator",
+                "int",
+                glob_domain_indices.dim,
+            ),
+            glob_domain_indices[tuple(0 for _ in range(glob_domain_indices.dim))],
+            glob_domain_indices[tuple(-1 for _ in range(glob_domain_indices.dim))],
+            flattened_halos,
+            periodicity,
+        )
+
+    def __call__(self, domain: DomainDescriptor) -> HaloContainer:
+        result = self.__wrapped_call__("__call__", domain)
+
+        local = union(
+            *(
+                ProductSet.from_coords(tuple(box2.local.first), tuple(box2.local.last))
+                for box2 in result
+            )
+        )
+        global_ = union(
+            *(
+                ProductSet.from_coords(tuple(box2.global_.first), tuple(box2.global_.last))
+                for box2 in result
+            )
+        )
+        return HaloContainer(local, global_)
