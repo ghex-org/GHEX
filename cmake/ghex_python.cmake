0f39873bc6a35a0f67eb56edf542081ea1199b92
boeschf <48126478+boeschf@users.noreply.github.com>
2024-03-25 14:09:19 +0100

    cleanup

--- cmake/ghex_python.cmake
+++ cmake/ghex_python.cmake
@@ -21,16 +21,12 @@ if (GHEX_BUILD_PYTHON_BINDINGS)
         ## update the environment with VIRTUAL_ENV variable (mimic the activate script)
         set (ENV{VIRTUAL_ENV} "${venv}")
         ## change the context of the search
-        set (Python3_FIND_VIRTUALENV ONLY)
+        set (Python3_FIND_VIRTUALENV FIRST)
         ## unset Python3_EXECUTABLE because it is also an input variable (see documentation, Artifacts Specification section)
         unset (Python3_EXECUTABLE)
         ## Launch a new search
         find_package (Python3 REQUIRED COMPONENTS Interpreter Development.Module)
     else()
-        #if(DEFINED PYTHON_EXECUTABLE)
-        #    set(Python3_EXECUTABLE ${PYTHON_EXECUTABLE})
-        #endif()
-        #set(Python_FIND_STRATEGY LOCATION)
         find_package (Python3 REQUIRED COMPONENTS Interpreter Development.Module)
     endif()
 
