a1d712c20e60dc2577c7b04c9ba1d73592512e65
boeschf <48126478+boeschf@users.noreply.github.com>
2024-12-20 10:03:58 +0100

    review suggestion: improve find_python_module cmake script to include version

--- cmake/ghex_find_python_module.cmake
+++ cmake/ghex_find_python_module.cmake
@@ -15,13 +15,19 @@ function(find_python_module module)
             RESULT_VARIABLE _${module}_status
             OUTPUT_VARIABLE _${module}_location
             ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
+            "import re, ${module}; print(re.compile('/__init__.py.*').sub('',${module}.__version__))"
+            OUTPUT_VARIABLE _${module}_version
+            ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
         message(STATUS "python-exe = ${PYTHON_EXECUTABLE}")
         message(STATUS "status = ${_${module}_status}")
         message(STATUS "location = ${_${module}_location}")
+        message(STATUS "version = ${_${module}_version}")
 
         if(NOT _${module}_status)
             set(HAVE_${module_upper} ON CACHE INTERNAL "Python module available")
             set(PY_${module_upper} ${_${module}_location} CACHE STRING "Location of Python module ${module}")
+            set(PY_${module_upper}_VERSION ${_${module}_version} CACHE STRING "Version of Python module ${module}")
         else()
             set(HAVE_${module_upper} OFF CACHE INTERNAL "Python module available")
         endif()
