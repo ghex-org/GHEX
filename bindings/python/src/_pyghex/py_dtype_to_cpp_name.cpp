fa0e22771417117f01d7248ea2558a790ac8ae13
Till Ehrengruber <till.ehrengruber@cscs.ch>
2025-01-04 18:15:52 +0100

    Add tests

--- bindings/python/src/_pyghex/py_dtype_to_cpp_name.cpp
+++ bindings/python/src/_pyghex/py_dtype_to_cpp_name.cpp
@@ -1,7 +1,7 @@
 /*
  * ghex-org
  *
- * Copyright (c) 2014-2024, ETH Zurich
+ * Copyright (c) 2014-2025, ETH Zurich
  * All rights reserved.
  *
  * Please, refer to the LICENSE file in the root directory.
