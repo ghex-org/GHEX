26f4f3e578a450c6758c17e55eb7210e7975fc58
Mikael Simberg <simberg@cscs.ch>
2026-01-06 13:00:43 +0100

    Remove unused functions in packer

--- include/ghex/packer.hpp
+++ include/ghex/packer.hpp
@@ -28,31 +28,6 @@ namespace ghex
 template<typename Arch>
 struct packer
 {
-    // TODO: Remove this overload?
-    template<typename Map, typename Requests, typename Communicator>
-    static void pack(Map& map, Requests& send_reqs, Communicator& comm)
-    {
-        for (auto& p0 : map.send_memory)
-        {
-            const auto device_id = p0.first;
-            for (auto& p1 : p0.second)
-            {
-                if (p1.second.size > 0u)
-                {
-                    if (!p1.second.buffer || p1.second.buffer.size() != p1.second.size)
-                        p1.second.buffer =
-                            arch_traits<Arch>::make_message(comm, p1.second.size, device_id);
-                    device::guard g(p1.second.buffer);
-                    auto          data = g.data();
-                    for (const auto& fb : p1.second.field_infos)
-                        // Directly call pack functionality instead of going through callback?
-                        fb.call_back(data + fb.offset, *fb.index_container, nullptr);
-                    // TODO: Only do packing, don't mix sending here. Good idea?
-                }
-            }
-        }
-    }
-
     template<typename Buffer>
     static void pack(Buffer& buffer, unsigned char* data)
     {
@@ -126,36 +101,6 @@ pack_kernel_u(device::kernel_argument<PackIterationSpace, N> args)
 template<>
 struct packer<gpu>
 {
-    // TODO: Remove this overload?
-    template<typename Map, typename Requests, typename Communicator>
-    static void pack(Map& map, Requests& send_reqs, Communicator& comm)
-    {
-        for (auto& p0 : map.send_memory)
-        {
-            const auto device_id = p0.first;
-            for (auto& p1 : p0.second)
-            {
-                if (p1.second.size > 0u)
-                {
-                    if (!p1.second.buffer || p1.second.buffer.size() != p1.second.size ||
-                        p1.second.buffer.device_id() != device_id)
-                    {
-                        p1.second.buffer =
-                            arch_traits<gpu>::make_message(comm, p1.second.size, device_id);
-                    }
-
-                    for (const auto& fb : p1.second.field_infos)
-                    {
-                        device::guard g(p1.second.buffer);
-                        fb.call_back(g.data() + fb.offset, *fb.index_container,
-                            (void*)(&p1.second.m_stream.get()));
-                    }
-                }
-            }
-        }
-        // TODO: Don't trigger send here, but in caller?
-    }
-
     template<typename Buffer>
     static void pack(Buffer& buffer, unsigned char* data)
     {
