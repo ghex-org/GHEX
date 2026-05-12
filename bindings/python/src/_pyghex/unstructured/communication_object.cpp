8f51fbd3be5f9f54ccef0b62c3217829266d30cf
Mikael Simberg <mikael.simberg@iki.fi>
2026-05-12 09:48:36 +0200

    Migrate from pybind11 to nanobind (#198)

--- bindings/python/src/_pyghex/unstructured/communication_object.cpp
+++ bindings/python/src/_pyghex/unstructured/communication_object.cpp
@@ -24,6 +24,10 @@
 #include <unstructured/field_descriptor.hpp>
 #include <unstructured/communication_object.hpp>
 
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/vector.h>
+#include <nanobind/stl/string.h>
+
 namespace pyghex
 {
 namespace unstructured
@@ -32,7 +36,7 @@ namespace
 {
 #if defined(GHEX_CUDACC)
 cudaStream_t
-extract_cuda_stream(pybind11::object python_stream)
+extract_cuda_stream(nanobind::object python_stream)
 {
     static_assert(std::is_pointer<cudaStream_t>::value);
     if (python_stream.is_none())
@@ -42,56 +46,56 @@ extract_cuda_stream(pybind11::object python_stream)
     }
     else
     {
-        if (pybind11::hasattr(python_stream, "__cuda_stream__"))
+        if (nanobind::hasattr(python_stream, "__cuda_stream__"))
         {
             // CUDA stream protocol: https://nvidia.github.io/cuda-python/cuda-core/latest/interoperability.html#cuda-stream-protocol
-            pybind11::tuple cuda_stream_protocol =
-                pybind11::getattr(python_stream, "__cuda_stream__")();
+            nanobind::tuple cuda_stream_protocol =
+                nanobind::cast<nanobind::tuple>(python_stream.attr("__cuda_stream__")());
             if (cuda_stream_protocol.size() != 2)
             {
                 std::stringstream error;
                 error << "Expected a tuple of length 2, but got one with length "
                       << cuda_stream_protocol.size();
-                throw pybind11::type_error(error.str());
+                throw nanobind::type_error(error.str().c_str());
             }
 
-            //Currently there is only version 0.
-            const auto protocol_version = cuda_stream_protocol[0].cast<std::size_t>();
+            const auto protocol_version = nanobind::cast<std::size_t>(cuda_stream_protocol[0]);
             if (protocol_version != 0)
             {
                 std::stringstream error;
                 error << "Expected `__cuda_stream__` protocol version 0, but got "
                       << protocol_version;
-                throw pybind11::type_error(error.str());
+                throw nanobind::type_error(error.str().c_str());
             }
 
-            const auto stream_address = cuda_stream_protocol[1].cast<std::uintptr_t>();
+            const auto stream_address = nanobind::cast<std::uintptr_t>(cuda_stream_protocol[1]);
             return reinterpret_cast<cudaStream_t>(stream_address);
         }
-        else if (pybind11::hasattr(python_stream, "ptr"))
+        else if (nanobind::hasattr(python_stream, "ptr"))
         {
             // CuPy stream: See https://docs.cupy.dev/en/latest/reference/generated/cupy.cuda.Stream.html#cupy-cuda-stream
-            std::uintptr_t stream_address = python_stream.attr("ptr").cast<std::uintptr_t>();
+            std::uintptr_t stream_address =
+                nanobind::cast<std::uintptr_t>(python_stream.attr("ptr"));
             return reinterpret_cast<cudaStream_t>(stream_address);
         }
         // TODO: Find out of how to extract the typename, i.e. `type(python_stream).__name__`.
         std::stringstream error;
         error << "Failed to convert the stream object into a CUDA stream.";
-        throw pybind11::type_error(error.str());
+        throw nanobind::type_error(error.str().c_str());
     }
 }
 #endif
 } // namespace
 
 void
-register_communication_object(pybind11::module& m)
+register_communication_object(nanobind::module_& m)
 {
     gridtools::for_each<
         gridtools::meta::transform<gridtools::meta::list, communication_object_specializations>>(
         [&m](auto l)
         {
             using namespace std::string_literals;
-            using namespace pybind11::literals;
+            using namespace nanobind::literals;
 
             using type = gridtools::meta::first<decltype(l)>;
             using handle = typename type::handle_type;
@@ -106,9 +110,9 @@ register_communication_object(pybind11::module& m)
 #if defined(GHEX_CUDACC)
                 .def(
                     "schedule_wait",
-                    [](typename type::handle_type& h, pybind11::object python_stream)
+                    [](typename type::handle_type& h, nanobind::object python_stream)
                     { return h.schedule_wait(extract_cuda_stream(python_stream)); },
-                    pybind11::keep_alive<0, 1>())
+                    nanobind::arg("stream").none())
 #endif
                 .def("is_ready", &handle::is_ready)
                 .def("progress", &handle::progress);
@@ -124,52 +128,52 @@ register_communication_object(pybind11::module& m)
                         .def(
                             "exchange", [](type& co, std::vector<buffer_info_type> b)
                             { return co.exchange(b.begin(), b.end()); },
-                            pybind11::keep_alive<0, 1>())
+                            nanobind::keep_alive<0, 1>())
                         .def(
                             "exchange", [](type& co, buffer_info_type& b)
-                            { return co.exchange(b); }, pybind11::keep_alive<0, 1>())
+                            { return co.exchange(b); }, nanobind::keep_alive<0, 1>())
                         .def(
                             "exchange", [](type& co, buffer_info_type& b0, buffer_info_type& b1)
-                            { return co.exchange(b0, b1); }, pybind11::keep_alive<0, 1>())
+                            { return co.exchange(b0, b1); }, nanobind::keep_alive<0, 1>())
                         .def(
                             "exchange",
                             [](type& co, buffer_info_type& b0, buffer_info_type& b1,
                                 buffer_info_type& b2) { return co.exchange(b0, b1, b2); },
-                            pybind11::keep_alive<0, 1>())
+                            nanobind::keep_alive<0, 1>())
 #if defined(GHEX_CUDACC)
                         .def(
                             "schedule_exchange",
-                            [](type& co, pybind11::object python_stream,
+                            [](type& co, nanobind::object python_stream,
                                 std::vector<buffer_info_type> b) {
                                 return co.schedule_exchange(extract_cuda_stream(python_stream),
                                     b.begin(), b.end());
                             },
-                            pybind11::keep_alive<0, 1>(), pybind11::arg("stream"),
-                            pybind11::arg("patterns"))
+                            nanobind::keep_alive<0, 1>(), nanobind::arg("stream").none(),
+                            nanobind::arg("patterns"))
                         .def(
                             "schedule_exchange",
-                            [](type& co, pybind11::object python_stream, buffer_info_type& b)
+                            [](type& co, nanobind::object python_stream, buffer_info_type& b)
                             { return co.schedule_exchange(extract_cuda_stream(python_stream), b); },
-                            pybind11::keep_alive<0, 1>(), pybind11::arg("stream"),
-                            pybind11::arg("b"))
+                            nanobind::keep_alive<0, 1>(), nanobind::arg("stream").none(),
+                            nanobind::arg("b"))
                         .def(
                             "schedule_exchange",
-                            [](type& co, pybind11::object python_stream, buffer_info_type& b0,
+                            [](type& co, nanobind::object python_stream, buffer_info_type& b0,
                                 buffer_info_type& b1) {
                                 return co.schedule_exchange(extract_cuda_stream(python_stream), b0,
                                     b1);
                             },
-                            pybind11::keep_alive<0, 1>(), pybind11::arg("stream"),
-                            pybind11::arg("b0"), pybind11::arg("b1"))
+                            nanobind::keep_alive<0, 1>(), nanobind::arg("stream").none(),
+                            nanobind::arg("b0"), nanobind::arg("b1"))
                         .def(
                             "schedule_exchange",
-                            [](type& co, pybind11::object python_stream, buffer_info_type& b0,
+                            [](type& co, nanobind::object python_stream, buffer_info_type& b0,
                                 buffer_info_type& b1, buffer_info_type& b2) {
                                 return co.schedule_exchange(extract_cuda_stream(python_stream), b0,
                                     b1, b2);
                             },
-                            pybind11::keep_alive<0, 1>(), pybind11::arg("stream"),
-                            pybind11::arg("b0"), pybind11::arg("b1"), pybind11::arg("b2"))
+                            nanobind::keep_alive<0, 1>(), nanobind::arg("stream").none(),
+                            nanobind::arg("b0"), nanobind::arg("b1"), nanobind::arg("b2"))
                         .def("has_scheduled_exchange",
                             [](type& co) -> bool { return co.has_scheduled_exchange(); })
 #endif // end scheduled exchange
@@ -178,7 +182,7 @@ register_communication_object(pybind11::module& m)
 
             m.def(
                 "make_co_unstructured", [](context_shim& c) { return type{c.m}; },
-                pybind11::keep_alive<0, 1>());
+                nanobind::keep_alive<0, 1>());
 
             m.def("expose_cpp_ptr",
                 [](type* obj) { return reinterpret_cast<std::uintptr_t>(obj); });
